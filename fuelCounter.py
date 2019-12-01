d5ce3017f699b4717eeff7f436c4bbf88b7f7fe1
resb53 <resb53.git@gmail.com>
2019-12-01 21:52:11 +0000

    First attempt at counting fuel

---
+++ fuelCounter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+
+# Check correct usage
+if (len(sys.argv) != 2):
+    sys.exit("USAGE: " + sys.argv[0] + " inputs_filename")
+
+# Parse input
+try:
+    masses_fh = open(sys.argv[1],'r')
+except IOError:
+    sys.exit("Unable to open input file: " + sys.argv[1])
+
+masses = []
+
+for line in masses_fh:
+    masses.append(int(line.rsplit()[0]))
+
+# Calculate fuel requirement based on day 1 task 1 rules
+total_fuel = 0;
+
+for mass in masses:
+    total_fuel += int(mass / 3) - 2#div by 3, round down, subtract 2
+
+print('Total fuel required: ' + str(total_fuel))
