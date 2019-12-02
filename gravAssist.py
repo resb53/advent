b24562c134590c700bbfc55fb5f47e91fc0e2c8b
resb53 <resb53.git@gmail.com>
2019-12-02 06:00:49 +0000

    Initial version

---
+++ gravAssist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import sys
+
+# Check correct usage
+if (len(sys.argv) != 2):
+    sys.exit("USAGE: " + sys.argv[0] + " inputs_filename")
+
+# Parse input
+try:
+    instr_fh = open(sys.argv[1],'r')
+except IOError:
+    sys.exit("Unable to open input file: " + sys.argv[1])
+
+# Parse instructions in file. Allow for multiline just in case future need.
+for line in instr_fh:
+    cmd = line.rsplit()[0]
+
+    # Split command string into list of instructions
+    instr = cmd.split(',')
+    instr = [int(x) for x in instr]
+    print("Input: " + str(instr))
+
+    # Reset after alarm
+    instr[1] = 12
+    instr[2] = 2
+    print("Reset: " + str(instr))
+
+    # Iterate through list
+    i = 0;
+
+    while (instr[i] != 99):
+        print("i=" + str(i))
+        print(instr[i:i+4])
+        # Decide operation based of value in first field
+        if (instr[i] == 1):
+            # Add values
+            instr[instr[i+3]] = instr[i+1] + instr[i+2]
+        elif (instr[i] == 2):
+            instr[instr[i+3]] = instr[i+1] * instr[i+2]
+        else:
+            sys.exit("Invalid operator in position " + str(i) + ": " + instr[i])
+        print("putting " + str(instr[instr[i+3]]) + " into " + str(instr[i+3]))
+        print("Update: " + str(instr))
+        i += 4
+
+    print("Output: " + str(instr))
