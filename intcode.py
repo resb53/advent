889fd92560a235bad278c64043e4888ef250143a
resb53 <resb53.git@gmail.com>
2019-12-02 06:55:57 +0000

    Refactor for new functions and find answer with grep

--- intcode.py
+++ intcode.py
@@ -6,41 +6,48 @@ import sys
 if (len(sys.argv) != 2):
     sys.exit("USAGE: " + sys.argv[0] + " inputs_filename")
 
-# Parse input
+# Parse memory
 try:
     instr_fh = open(sys.argv[1],'r')
 except IOError:
     sys.exit("Unable to open input file: " + sys.argv[1])
 
-# Parse instructions in file. Allow for multiline just in case future need.
-for line in instr_fh:
-    cmd = line.rsplit()[0]
+def main():
+    # Parse instructions in file. Allow for multiline just in case future need.
+    for line in instr_fh:
+        cmd = line.rsplit()[0]
 
-    # Split command string into list of instructions
-    instr = cmd.split(',')
-    instr = [int(x) for x in instr]
-    print("Input: " + str(instr))
+        # Split command string into list of instructions
+        initmem = cmd.split(',')
+        initmem = [int(x) for x in initmem]
 
-    # Reset after alarm
-    instr[1] = 12
-    instr[2] = 2
-    print("Reset: " + str(instr))
+        # Reset after alarm
+        #noun = 12
+        #verb = 2
 
+        # Set inputs and run
+        for noun in range(100):
+            for verb in range(100):
+                mem = initmem.copy() #reinitisalise
+                mem[1] = noun
+                mem[2] = verb
+                operate(mem)
+                print("Noun: " + str(noun) + ", Verb: " + str(verb) + ", Output: " + str(mem[0]))
+
+def operate(mem):
     # Iterate through list
     i = 0;
 
-    while (instr[i] != 99):
-        print("i=" + str(i))
-        print(instr[i:i+4])
+    while (mem[i] != 99):
         # Decide operation based of value in first field
-        if (instr[i] == 1):
+        if (mem[i] == 1):
             # Add values
-            instr[instr[i+3]] = instr[instr[i+1]] + instr[instr[i+2]]
-        elif (instr[i] == 2):
+            mem[mem[i+3]] = mem[mem[i+1]] + mem[mem[i+2]]
+        elif (mem[i] == 2):
             # Multiply values
-            instr[instr[i+3]] = instr[instr[i+1]] * instr[instr[i+2]]
+            mem[mem[i+3]] = mem[mem[i+1]] * mem[mem[i+2]]
         else:
-            sys.exit("Invalid operator in position " + str(i) + ": " + instr[i])
+            sys.exit("Invalid operator in position " + str(i) + ": " + str(mem[i]))
         i += 4
 
-    print("Output: " + str(instr))
+main()
