5bfec9deb70f266722425e843ceeadbea2d353b6
resb53 <resb53.git@gmail.com>
2019-12-04 05:15:42 +0000

    Initial version on day 4

---
+++ src/codeBreak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+# Check correct usage
+if (len(sys.argv) != 2):
+    sys.exit("USAGE: " + sys.argv[0] + " inputs_filename")
+
+# Parse memory
+try:
+    instr_fh = open(sys.argv[1],'r')
+except IOError:
+    sys.exit("Unable to open input file: " + sys.argv[1])
+
+def main():
+    # Parse instructions in file. Allow for multiline just in case future need.
+    for line in instr_fh:
+        rng = line.rsplit()[0]
+
+        # Split command string into list of instructions
+        [rmin, rmax] = rng.split('-') #min and max range for codes
+        rmin = int(rmin)
+        rmax = int(rmax)
+
+        
+
+main()
