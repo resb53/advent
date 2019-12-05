899af9f4488daa6cd0934cd78c81abdec7e659df
resb53 <resb53.git@gmail.com>
2019-12-05 05:32:03 +0000

    Added opcodes 3 and 4

--- src/intcode.py
+++ src/intcode.py
@@ -26,28 +26,41 @@ def main():
         #verb = 2
 
         # Set inputs and run
-        for noun in range(100):
-            for verb in range(100):
-                mem = initmem.copy() #reinitisalise
-                mem[1] = noun
-                mem[2] = verb
-                operate(mem)
-                print("Noun: " + str(noun) + ", Verb: " + str(verb) + ", Output: " + str(mem[0]))
+        #for noun in range(100):
+        #    for verb in range(100):
+        #        mem = initmem.copy() #reinitisalise
+        #        mem[1] = noun
+        #        mem[2] = verb
+        #        operate(mem)
+        #        print("Noun: " + str(noun) + ", Verb: " + str(verb) + ", Output: " + str(mem[0]))
+
+        operate(initmem)
 
 def operate(mem):
     # Iterate through list
     i = 0;
 
     while (mem[i] != 99):
+        #print("i:" + str(i) + "; mem:" + str(mem))
         # Decide operation based of value in first field
         if (mem[i] == 1):
             # Add values
             mem[mem[i+3]] = mem[mem[i+1]] + mem[mem[i+2]]
+            i += 4
         elif (mem[i] == 2):
             # Multiply values
             mem[mem[i+3]] = mem[mem[i+1]] * mem[mem[i+2]]
+            i += 4
+        elif (mem[i] == 3):
+            # Read input from STDIN. Save it to the address given.
+            print('Provide input: ', end='', flush=True)
+            inp = int(sys.stdin.readline().rsplit()[0])
+            mem[mem[i+1]] = inp
+            i += 2
+        elif (mem[i] == 4):
+            print(mem[mem[i+1]], end='')
+            i += 2
         else:
             sys.exit("Invalid operator in position " + str(i) + ": " + str(mem[i]))
-        i += 4
 
 main()
