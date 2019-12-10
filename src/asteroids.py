a048a7f39a48b45dfcb47997ff446e36a2332e5f
resb53 <resb53.git@gmail.com>
2019-12-10 06:13:38 +0000

    Radial iterator

--- src/asteroids.py
+++ src/asteroids.py
@@ -23,8 +23,72 @@ def main():
         line = line.rsplit()[0]
         asteroids.append(list(line))
 
-    print(asteroids)
+    #print(asteroids)
 
+    # Iterate through elements
+    x=0
+    y=0
+    maxast = 0
+
+    for row in asteroids:
+        for col in row:
+            if asteroids[y][x] == '#':
+                astseen = seeAsteroids(x, y, asteroids)
+                if astseen > maxast:
+                    maxast = astseen
+            x += 1
+        y += 1
+        x = 0
+
+    print(str(len(asteroids)) + ' ' + str(len(asteroids[0])))
+    print(maxast)
+
+def seeAsteroids(x, y, grid):
+    # Work out radially from this point, blocking off routes as you see asteroid.
+    count = 0
+
+    # Create list of relative positions to iterate through
+    radial = radialIterator(x, y, grid)
+
+    print("x:" + str(x) + "; y:" + str(y) + "; rad:" + str(radial))
+
+    return count
+
+def radialIterator(x, y, grid):
+    # Return a radial list of relative locations to iterate through, and each position has an indicator as to what else is blocked
+    dist = 1
+    rad = []
+
+    for dist in range(1,len(grid)+1): # This assumes at least a tall as it is wide!
+        j = -dist # top row
+        if y + j >= 0 and y + j < len(grid):
+            for i in range(-dist,dist):
+                if x + i >= 0 and x + i < len(grid[y]):
+                    rad.append([i,j])
+        i = dist # right side
+        if x + i >= 0 and x + i < len(grid[y]):
+            for j in range(-dist,dist):
+                if y + j >= 0 and y + j < len(grid):
+                    rad.append([i,j])
+        j = dist # bottom
+        if y + j >= 0 and y + j < len(grid):
+            for i in range(dist,-dist,-1):
+                if x + i >= 0 and x + i < len(grid[y]):
+                    rad.append([i,j])
+        i = -dist # left side
+        if x + i >= 0 and x + i < len(grid[y]):
+            for j in range(dist,-dist,-1):
+                if y + j >= 0 and y + j < len(grid):
+                    rad.append([i,j])
+
+    return rad
+
+def isAsteroid(x, y, grid):
+    # Is this an asteroid?
+    if grid[y][x] == '#':
+        return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
     main()
