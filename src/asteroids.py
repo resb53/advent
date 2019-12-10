c044b106bdbce58a18ab884d916201e98074900a
resb53 <resb53.git@gmail.com>
2019-12-10 06:57:20 +0000

    Calculates asteroids visible correctly

--- src/asteroids.py
+++ src/asteroids.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import os
 import sys
 import argparse
+from math import degrees, atan2
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Asteroid effectiveness calculator.")
@@ -40,17 +40,24 @@ def main():
         y += 1
         x = 0
 
-    print(str(len(asteroids)) + ' ' + str(len(asteroids[0])))
     print(maxast)
 
 def seeAsteroids(x, y, grid):
     # Work out radially from this point, blocking off routes as you see asteroid.
     count = 0
+    seen = [] # asteroid blocking a direction
 
     # Create list of relative positions to iterate through
     radial = radialIterator(x, y, grid)
 
-    print("x:" + str(x) + "; y:" + str(y) + "; rad:" + str(radial))
+    # Count asteroids seen
+    for check in radial:
+        if isAsteroid(x+check[0],y+check[1],grid):
+            if check[2] not in seen:
+                count += 1
+                seen.append(check[2])
+
+    #print("x:" + str(x) + "; y:" + str(y) + "; rad:" + str(radial))
 
     return count
 
@@ -64,22 +71,22 @@ def radialIterator(x, y, grid):
         if y + j >= 0 and y + j < len(grid):
             for i in range(-dist,dist):
                 if x + i >= 0 and x + i < len(grid[y]):
-                    rad.append([i,j])
+                    rad.append([i,j,round(degrees(atan2(i, j)),2)]) # Not correct bearings, but unique for a direction
         i = dist # right side
         if x + i >= 0 and x + i < len(grid[y]):
             for j in range(-dist,dist):
                 if y + j >= 0 and y + j < len(grid):
-                    rad.append([i,j])
+                    rad.append([i,j,round(degrees(atan2(i, j)),2)])
         j = dist # bottom
         if y + j >= 0 and y + j < len(grid):
             for i in range(dist,-dist,-1):
                 if x + i >= 0 and x + i < len(grid[y]):
-                    rad.append([i,j])
+                    rad.append([i,j,round(degrees(atan2(i, j)),2)])
         i = -dist # left side
         if x + i >= 0 and x + i < len(grid[y]):
             for j in range(dist,-dist,-1):
                 if y + j >= 0 and y + j < len(grid):
-                    rad.append([i,j])
+                    rad.append([i,j,round(degrees(atan2(i, j)),2)])
 
     return rad
 
