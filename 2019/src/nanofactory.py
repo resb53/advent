ff38d7636d8380cd57f71e91e67482d800042976
resb53 <resb53.git@gmail.com>
2019-12-14 16:50:58 +0000

    Works on first test, should use leftovers

--- 2019/src/nanofactory.py
+++ 2019/src/nanofactory.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 
 # Globals
-menu = {'ORE': [1, {'ORE': 1}]} # 1 ORE always requires 1 ORE
+menu = {}
 
 def main():
     # Check correct usage
@@ -45,24 +45,53 @@ def calculateOre():
     global menu
     # Count numbers required
     shoplist = {'FUEL': 1}
+    leftover = {} # Remaining components
 
     #Get 1 FUEL
     while list(shoplist.keys()) != ['ORE']:
-        print( str(list(shoplist.keys())) + ' vs ' + str(['ORE']) )
         print('S:' + str(shoplist))
         newlist = {}
         for item in shoplist:
+            if item == 'ORE':
+                newlist[item] = shoplist[item]
             print(item)
             for ing in menu[item][1]:
+                print('<' + ing)
+                # Get most efficient production from menu
+                [prod, used] = getReq(shoplist[item], menu[item][1][ing], menu[item][0])
+                print('Use: ' + str(used) + ' to make ' + str(prod))
+                # Update lists
                 if ing in newlist:
-                    newlist[ing] += shoplist[item] * menu[item][1][ing]
+                    newlist[ing] += used
                 else:
-                    newlist[ing] = shoplist[item] * menu[item][1][ing]
-            print(newlist)
+                    newlist[ing] = used
+                # Use leftovers first
+                if ing in leftover:
+                    if leftover[ing] < newlist[ing]:
+                        newlist[ing] -= leftover[ing]
+                    else:
+                        del(newlist[ing])
+                        leftover[ing] -= newlist[ing]
+                # Calculate leftovers
+                if prod > shoplist[item]:
+                    if item in leftover:
+                        leftover[item] += prod - shoplist[item]
+                    else:
+                        leftover[item] = prod - shoplist[item]
+
         shoplist = newlist
+        print('L:' + str(leftover))
 
     print(shoplist)
 
+def getReq(req, need, prod):
+    print('Req: ' + str(req) + '; Need: ' + str(need) + '; Prod: ' + str(prod))
+    used = 0
+    done = 0
+    while done < req:
+        used += need
+        done += prod
+    return [done, used]
 
 if __name__ == "__main__":
     main()
