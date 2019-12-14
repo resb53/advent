8f07391410a1821fe93569a7e270bdbbfc1505f4
resb53 <resb53.git@gmail.com>
2019-12-14 22:35:57 +0000

    Think this works... but slow

--- 2019/src/nanofactory.py
+++ 2019/src/nanofactory.py
@@ -15,6 +15,10 @@ def main():
     init(args.ingredients)
     # Menu ready
     recipe = calculateOre()
+    recipe = orderRecipe(recipe)
+    #printRecipe(recipe)
+    # Use recipe
+    cook(recipe)
 
 
 def init(fh):
@@ -41,7 +45,6 @@ def init(fh):
         menu[result] = [int(qr), mix]
 
 def calculateOre():
-    global menu
     # Count numbers required
     shoplist = {'FUEL': 1}
     recipe = [] # Production recipe to follow
@@ -106,18 +109,9 @@ def calculateOre():
 
     # PART ONE
     #print('ORE required: ' + str(ore))
-    # print recipe
-    #for entry in recipe:
-    #    print('Produce ' + str(entry[1]) + ' of ' + entry[0] + ' using:', end='')
-    #    crafts = entry[1] // menu[entry[0]][0]
-    #    for ing in menu[entry[0]][1]:
-    #        print(' ' + str(crafts * menu[entry[0]][1][ing]) + ' ' + ing + ',', end='')
-    #    print('')
-
     #print('Leftovers: ' + str(leftover))
 
     return recipe
-
 
 def getReq(req, need, prod, avail):
     #print('Req: ' + str(req) + '; Need: ' + str(need) + '; Prod: ' + str(prod) + '; Avail: ' + str(avail))
@@ -136,6 +130,66 @@ def getReq(req, need, prod, avail):
     #print('Use: ' + str(used) + ' to make ' + str(done) + '. Require: ' + str(new) + ' new and ' + str(avail) + ' leftover.')
 
     return [done, new, avail]
+
+def orderRecipe(recipe):
+    seen = ['ORE']
+    ordered = []
+    while len(ordered) < len(recipe):
+        for instr in recipe:
+            # if not already 'made'
+            if instr[0] not in seen:
+                mats = 0
+                for mat in menu[instr[0]][1]:
+                    if mat in seen:
+                        mats += 1
+                if mats == len(menu[instr[0]][1]):
+                    #We have the mats, let's cook
+                    ordered.append(instr)
+                    seen.append(instr[0])
+    return ordered
+
+def printRecipe(recipe):
+    for entry in recipe:
+        print('Produce ' + str(entry[1]) + ' of ' + entry[0] + ' using:', end='')
+        crafts = entry[1] // menu[entry[0]][0]
+        for ing in menu[entry[0]][1]:
+            print(' ' + str(crafts * menu[entry[0]][1][ing]) + ' ' + ing + ',', end='')
+        print('')
+
+def cook(recipe):
+    # Have a bag of goodies and cook up that tasty fuel
+    bag = {'ORE': 1000000000000}
+    #bag = {'ORE': 300000}
+    lp = 1000000000000 # last print
+    i = 10000000000 # print iterator
+    while bag['ORE'] > 0:
+        if len(bag) == 2:
+            print(bag)
+        if bag['ORE'] <= lp -i:
+            print(bag)
+            lp -= i
+        for instr in recipe:
+            # See if we have enough already
+            if instr[0] in bag and instr[0] != 'FUEL' and bag[instr[0]] >= instr[1]:
+                #print('Already enough ' + instr[0] + ' in bag.')
+                q = 0
+            else:
+                #print('Cooking: ' + str(instr[1]) + ' ' + str(instr[0]) + ' using: ' + str(menu[instr[0]]))
+                for mat in menu[instr[0]][1]:
+                    req = instr[1] // menu[instr[0]][0] * menu[instr[0]][1][mat]
+                    if mat in bag:
+                        bag[mat] -= req
+                        #print ('- Used ' + str(req) + ' ' + mat)
+                        if bag[mat] < 0:
+                            sys.exit('Not enough ' + mat + ' in bag.\nBag: ' + str(bag))
+                    else:
+                        sys.exit('No ' + mat + ' in bag.')
+                # crafted
+                if instr[0] not in bag:
+                    bag[instr[0]] = instr[1]
+                else:
+                    bag[instr[0]] += instr[1]
+                #print('& Bag: ' + str(bag))
 
 if __name__ == "__main__":
     main()
