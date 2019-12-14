fae60879ebbeeb1e3dc0218d6acb4a45a0c8952a
resb53 <resb53.git@gmail.com>
2019-12-14 16:21:19 +0000

    Doesn't account for producing more than 1 of an item, or holding onto left overs

--- 2019/src/nanofactory.py
+++ 2019/src/nanofactory.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 
 # Globals
-menu = {}
+menu = {'ORE': [1, {'ORE': 1}]} # 1 ORE always requires 1 ORE
 
 def main():
     # Check correct usage
@@ -13,7 +13,10 @@ def main():
     args = parser.parse_args()
 
     init(args.ingredients)
+    # Menu ready
     print(menu)
+    calculateOre()
+
 
 def init(fh):
     global menu
@@ -34,9 +37,32 @@ def init(fh):
         mix = {}
         for mat in mats.split(', '):
             qm, mat = mat.split(' ')
-            mix[mat] = qm
+            mix[mat] = int(qm)
 
-        menu[result] = [qr, mix]
+        menu[result] = [int(qr), mix]
+
+def calculateOre():
+    global menu
+    # Count numbers required
+    shoplist = {'FUEL': 1}
+
+    #Get 1 FUEL
+    while list(shoplist.keys()) != ['ORE']:
+        print( str(list(shoplist.keys())) + ' vs ' + str(['ORE']) )
+        print('S:' + str(shoplist))
+        newlist = {}
+        for item in shoplist:
+            print(item)
+            for ing in menu[item][1]:
+                if ing in newlist:
+                    newlist[ing] += shoplist[item] * menu[item][1][ing]
+                else:
+                    newlist[ing] = shoplist[item] * menu[item][1][ing]
+            print(newlist)
+        shoplist = newlist
+
+    print(shoplist)
+
 
 if __name__ == "__main__":
     main()
