36a44d1253f646f8005926c3e18bbb7751b7d9a5
resb53 <resb53.git@gmail.com>
2019-12-15 12:46:05 +0000

    Automate, with random - make revisit unchecked

--- 2019/src/oxybot.py
+++ 2019/src/oxybot.py
@@ -7,11 +7,13 @@ import time
 import tty
 import termios
 import signal
+from random import randint
 
 pos = [0,0] # x,y
 prev = [0,0]
 start = [0,0]
 grid = {} # grid[y][x] -> 0 empty, 1 wall, 2 robot
+around = {} # whats currently around the bot
 io = {'input': [], 'output': []} # Dict holding next input / last output
 move = {'w': 1, 's': 2, 'a': 3, 'd':4}
 draw = {0: '█', 1: 'o', 2: '!', 3: '.', 4: 'x'} # in the new space draw a wall, or the new droid pos, or the oxysys
@@ -60,15 +62,19 @@ def drawin():
 
 def instr_in():
     k = 'x'
-    while k not in move:
-        k = sys.stdin.read(1)
+    # Manual Play
+    #while k not in move:
+    #    k = sys.stdin.read(1)
+    # Auto Play
+    k = generateMove()
+
     moveFocus(move[k])
     return move[k]
 
 def instr_out(p):
     grid[pos[1]][pos[0]] = p
-    if p == 0:
-        # If it was a wall, keep droid in pos, reset pos
+    if p == 0 or p == 2:
+        # If it was a wall, or the oxytank, keep droid in pos, reset pos
         pos[0] = prev[0]
         pos[1] = prev[1]
     else:
@@ -80,6 +86,7 @@ def instr_out(p):
         else:
             grid[prev[1]][prev[0]] = 3
 
+    updateSur()
     drawin()
 
 def moveFocus(k):
@@ -98,6 +105,45 @@ def moveFocus(k):
     if pos[1] not in grid:
         grid[pos[1]] = {}
     grid[pos[1]][pos[0]] = ''
+
+def generateMove():
+    # Use around to pick next move
+    for d in around:
+        if around[d] == None:
+            return d
+    else:
+        return list(move.keys())[randint(0,3)]
+
+def updateSur():
+    global around
+    check = [0, 0]
+    check[0] = pos[0]
+    check[1] = pos[1]
+    # w
+    check[1] -= 1
+    around['w'] = getTile(check)
+    # s
+    check[1] += 2
+    around['s'] = getTile(check)
+    # a
+    check[1] -= 1
+    check[0] -= 1
+    around['a'] = getTile(check)
+    # d
+    check[0] += 2
+    around['d'] = getTile(check)
+    # print
+    #global win
+    #win.addstr(0, 0, '                                                      ')
+    #win.addstr(0, 0, str(around))
+    #win.refresh
+
+def getTile(check):
+    if check[1] in grid and check[0] in grid[check[1]]:
+        return grid[check[1]][check[0]]
+    else:
+        return None
+
 
 if __name__ == "__main__":
     main()
