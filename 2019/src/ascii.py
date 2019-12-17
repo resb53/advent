64ef8dc5028a993fca8c49898835e71a6fb0e34e
resb53 <resb53.git@gmail.com>
2019-12-17 19:10:11 +0000

    Bug fix in intCode and win the day by hand

--- 2019/src/ascii.py
+++ 2019/src/ascii.py
@@ -7,16 +7,27 @@ import time
 
 pos = [0,0] # x, y
 grid = {} # grid[y][x]
+instr = [   'A',',','B',',','A',',','C',',','B',',','C',',','B',',','A',',','C',',','B','\n',
+            'L',',','6',',','R',',','8',',','R',',','1','2',',','L',',','6',',','L',',','8','\n',
+            'L',',','1','0',',','L',',','8',',','R',',','1','2','\n',
+            'L',',','8',',','L',',','1','0',',','L',',','6',',','L',',','6','\n',
+            'n','\n'    ]
 
 def main():
     # Prepare intcode computer
     prog = intCode.Program('inputs/ascii.txt')
-    prog.run(o=instr_out)
     # Do part 1: Find intersections and calculate alignment parameters
-    cleanGrid()
-    printGrid()
+    #prog.run(o=instr_out)
+    #cleanGrid()
+    #printGrid()
     #intersect()
     # Do part 2:
+    prog.mem[0] = 2
+    prog.run(i=instr_in)
+
+def instr_in():
+    global instr
+    return ord(instr.pop(0))
 
 def instr_out(p):
     global pos, grid
