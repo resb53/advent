06b7c68bc1afc70dd274d8137f0bb478e3987bad
resb53 <resb53.git@gmail.com>
2020-12-01 08:57:49 +0000

    Quick solution to part 2, but I want to tidy this up!

--- 2020/src/day-01.py
+++ 2020/src/day-01.py
@@ -16,6 +16,7 @@ def main():
     global accounts
     parseInput(args.input)
     findErrors()
+    findThree()
 
 
 # Parse the input file
@@ -42,7 +43,8 @@ def findErrors():
         add = 5000
         check = size - 1
 
-        while add > 2020:
+        # Sum until we're redoing pairs
+        while add > 2020 and accounts[i] < accounts[check]:
             add = accounts[i] + accounts[check]
             #print (str(accounts[i]) + ' + ' + str(accounts[check]) + ' = ' + str(add))
             check = check - 1
@@ -50,9 +52,30 @@ def findErrors():
         #print(str(add) + ' = ' + str(accounts[i]) + ' + ' + str(accounts[check + 1]))
         if add == 2020:
             print('2020 = ' + str(accounts[i]) + ' + ' + str(accounts[check + 1]))
-            print('Answer is: ' + str(accounts[i] * accounts[check + 1]))
+            print('Part 1 answer is: ' + str(accounts[i] * accounts[check + 1]))
             break
 
+
+# Part 2, iterate through from low to high, then do the same checks above for the next element and countback
+def findThree():
+    size = len(accounts)
+
+    for i in range(size):
+        for j in range(i+1, size):
+            add = 5000
+            check = size - 1
+
+            # Sum until we're redoing pairs
+            while add > 2020 and accounts[j] < accounts[check]:
+                add = accounts[i] + accounts[j] + accounts[check]
+                #print (str(accounts[i]) + ' + ' + str(accounts[check]) + ' = ' + str(add))
+                check = check - 1
+
+            #print(str(add) + ' = ' + str(accounts[i]) + ' + ' + str(accounts[check + 1]))
+            if add == 2020:
+                print('2020 = ' + str(accounts[i]) + ' + ' + str(accounts[j]) + ' + ' + str(accounts[check + 1]))
+                print('Part 2 answer is: ' + str(accounts[i] * accounts[j] * accounts[check + 1]))
+                break
 
 if __name__ == "__main__":
     main()
