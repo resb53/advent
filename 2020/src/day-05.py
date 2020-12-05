d63f8608acdf9adf90d183685e5b7d61980da1d8
resb53 <resb53.git@gmail.com>
2020-12-05 07:49:17 +0000

    Solution to day 5, part 1

--- 2020/src/day-05.py
+++ 2020/src/day-05.py
@@ -10,6 +10,8 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 passes = []
+rows = 128
+cols = 8
 
 
 def main():
@@ -21,7 +23,7 @@ def main():
     # Part 2
 
     # Debug
-    printSeats()
+    # printSeats()
 
 
 # Parse the input file
@@ -38,7 +40,34 @@ def parseInput(inp):
 
 # For each pass, identify its seat
 def findSeats():
-    return True
+    maxseat = 0
+
+    for item in passes:
+        minrow = 0
+        maxrow = rows - 1
+        mincol = 0
+        maxcol = cols - 1
+
+        for char in item:
+            if char == 'F':
+                maxrow = int((maxrow - minrow)/2) + minrow
+            elif char == 'B':
+                minrow = int((maxrow - minrow)/2) + 1 + minrow
+            if char == 'L':
+                maxcol = int((maxcol - mincol)/2) + mincol
+            elif char == 'R':
+                mincol = int((maxcol - mincol)/2) + 1 + mincol
+
+        if minrow != maxrow or mincol != maxcol:
+            sys.exit(f"Error for pass {item}: Item: {item}, Minrow: {minrow},",
+                     f"Maxrow: {maxrow}, Mincol: {mincol}, Maxcol: {maxcol}")
+
+        # Calculate seat
+        seat = minrow * 8 + mincol
+        if seat > maxseat:
+            maxseat = seat
+    
+    print(maxseat)
 
 
 def printSeats():
