4dbe7854ec56d2e20ce0809b15e1d858a5f1f41a
resb53 <resb53.git@gmail.com>
2020-12-08 08:05:37 +0000

    Refactor day 08 to use external Handheld class for computer

--- 2020/src/day-08.py
+++ 2020/src/day-08.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+from handheld import Handheld
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Check your Code.")
@@ -10,18 +11,17 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 incode = []
-acc = 0
-exo = []  # Execution Order
 
 
 def main():
     parseInput(args.input)
 
     # Part 1
-    executeCode(incode)
+    cpu = Handheld(incode)
+    cpu.execute()
 
     # Part 2
-    findBug()
+    findBug(cpu)
 
     # Debug
     # printCode()
@@ -40,64 +40,29 @@ def parseInput(inp):
         incode.append([op, int(arg)])
 
 
-# For each pass, identify its seat
-def executeCode(code):
-    global acc, exo
-    acc = 0
-    step = 0
-    exo = []
-
-    while step not in exo:
-        exo.append(step)
-        # Exit gracefully
-        if step < 0 or step >= len(code):
-            return 0
-
-        op = code[step][0]
-        arg = code[step][1]
-
-        # Execute step
-        if op == 'acc':
-            acc += arg
-            step += 1
-
-        elif op == 'jmp':
-            step = step + arg
-
-        elif op == 'nop':
-            step += 1
-
-        else:
-            sys.exit(f"Unknown argument on line {step}: {arg}")
-
-    print(f"Infinite loop at instr: {step}, acc: {acc}", file=sys.stderr)
-    return 1
-
-
 # Find bug
-def findBug():
-    testcode = incode.copy()
-
+def findBug(cpu):
     # list all potention ops to change
     for step in range(len(incode)):
         test = False
 
-        if testcode[step][0] == 'jmp':
+        if incode[step][0] == 'jmp':
             reset = 'jmp'
-            testcode[step][0] = 'nop'
+            incode[step][0] = 'nop'
             test = True
-        elif testcode[step][0] == 'nop':
+        elif incode[step][0] == 'nop':
             reset = 'nop'
-            testcode[step][0] = 'jmp'
+            incode[step][0] = 'jmp'
             test = True
 
         if test:
-            if executeCode(testcode) == 0:
-                print(f"Ended gracefully, acc: {acc}")
+            cpu.load(incode)
+            if cpu.execute() == 0:
+                print(f"Ended gracefully, acc: {cpu.acc}")
                 return 0  # Bug found
 
             else:
-                testcode[step][0] = reset
+                incode[step][0] = reset
 
     return 1  # Bug not found
 
---
+++ 2020/src/handheld.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+
+
+class Handheld:
+    'A class for running Handheld assembly'
+
+    def __init__(self, incode):
+        self.code = incode
+        self.acc = 0
+        self.ptr = 0
+        self.exo = []
+
+    def load(self, newcode):
+        self.code = newcode
+        return 0
+
+    # For each pass, identify its seat
+    def execute(self, start=0):
+        self.acc = 0
+        self.ptr = start
+        self.exo = []
+
+        while self.ptr not in self.exo:
+            self.exo.append(self.ptr)
+
+            # Exit gracefully
+            if self.ptr == len(self.code):
+                return 0
+
+            op = self.code[self.ptr][0]
+            arg = self.code[self.ptr][1]
+
+            # Execute step
+            if op == 'acc':
+                self.acc += arg
+                self.ptr += 1
+
+            elif op == 'jmp':
+                self.ptr += arg
+
+            elif op == 'nop':
+                self.ptr += 1
+
+            else:
+                print(f"Unknown argument at pointer {self.ptr}: {arg}",
+                      file=sys.stderr)
+                return 1
+
+        print(f"Infinite loop at pointer {self.ptr}, acc: {self.acc}",
+              file=sys.stderr)
+        return 2
