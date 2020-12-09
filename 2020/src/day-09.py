cebe879f853b4b6ee91d92a8cbcca3eccbfc609c
resb53 <resb53.git@gmail.com>
2020-12-09 05:55:01 +0000

    Day 9, getting started

--- 2020/src/day-09.py
+++ 2020/src/day-09.py
@@ -2,47 +2,64 @@
 
 import argparse
 import sys
+from itertools import combinations
 
 # Check correct usage
-parser = argparse.ArgumentParser(description="Check your Passport.")
+parser = argparse.ArgumentParser(description="Check your Datastream.")
 parser.add_argument('input', metavar='input', type=str,
-                    help='Password list input.')
+                    help='Data input.')
 args = parser.parse_args()
 
-passes = []
+data = []
 
 
 def main():
     parseInput(args.input)
 
     # Part 1
-    findSeats()
+    findFirstError()
 
     # Part 2
 
     # Debug
-    printSeats()
+    # printSeats()
 
 
 # Parse the input file
 def parseInput(inp):
-    global passes
+    global data
     try:
-        passes_fh = open(inp, 'r')
+        data_fh = open(inp, 'r')
     except IOError:
         sys.exit("Unable to open input file: " + inp)
 
-    for line in passes_fh:
-        passes.append(line.strip("\n"))
+    for line in data_fh:
+        data.append(int(line.strip("\n")))
 
 
 # For each pass, identify its seat
-def findSeats():
-    return True
+def findFirstError():
+    # Step through windows of 25 numbers
+    for i in range(25, len(data)):
+        window = data[i-25:i]
+
+        # Sum until finding a match
+        combs = list(combinations(window, 2))
+        match = False
+
+        while match is False and len(combs) > 0:
+            check = combs.pop(0)
+
+            if sum(check) == data[i]:
+                match = True
+
+        if match is False:
+            return data[i]
+
 
 
 def printSeats():
-    for item in passes:
+    for item in data:
         print(item)
 
 
