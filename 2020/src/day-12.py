f447ab73a7d1bb2cfc4661709cc02b7e070f0bf4
resb53 <resb53.git@gmail.com>
2020-12-12 09:21:55 +0000

    Solution to day 12, part 1

---
+++ 2020/src/day-12.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+
+# Check correct usage
+parser = argparse.ArgumentParser(description="Check your Bearing.")
+parser.add_argument('input', metavar='input', type=str,
+                    help='Sea navigation input.')
+args = parser.parse_args()
+
+navigation = []
+pos = complex()  # positive real = E, positive imag = N
+waypoint = 10 + 1j  # 10 E, 1 N
+face = 0  # degrees = E
+faceDir = {0: 'E', 90: 'N', 180: 'W', 270: 'S'}
+
+
+def main():
+    parseInput(args.input)
+
+    # Part 1
+    findPosition()
+    print(pos)
+    print(abs(pos.real) + abs(pos.imag))
+
+    # Part 2
+    moveViaWaypoint()
+
+    # Debug
+    # printNavigation()
+
+
+# Parse the input file
+def parseInput(inp):
+    global navigation
+    try:
+        navi_fh = open(inp, 'r')
+    except IOError:
+        sys.exit("Unable to open input file: " + inp)
+
+    for line in navi_fh:
+        line = line.strip("\n")
+        navigation.append((line[0], int(line[1:])))
+
+
+# For each pass, identify its seat
+def findPosition():
+    global face
+    # Move based on command
+    for move in navigation:
+        cmd = move[0]
+        arg = move[1]
+
+        if cmd == 'L':
+            face = (face + arg) % 360
+        elif cmd == 'R':
+            face = (face - arg) % 360
+        elif cmd == 'F':
+            moveShip(faceDir[face], arg)
+        else:
+            moveShip(cmd, arg)
+
+
+# Move ship based on cmd and arg
+def moveShip(cmd, arg):
+    global pos
+
+    if cmd == 'N':
+        pos = pos + arg * 1j
+    elif cmd == 'S':
+        pos = pos - arg * 1j
+    elif cmd == 'E':
+        pos = pos + arg
+    elif cmd == 'W':
+        pos = pos - arg
+    else:
+        sys.exit(f"Cannot follow move cmd: {cmd}")
+
+
+# Move waypoint and move towards it
+def moveViaWaypoint():
+    global waypoint, face
+    face = 0  # reset
+
+    return True
+
+
+def printNavigation():
+    for item in navigation:
+        print(item)
+
+
+if __name__ == "__main__":
+    main()
