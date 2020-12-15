82c127d6a0615d06c8a2c96f8e4ee6eb43392e8c
resb53 <resb53.git@gmail.com>
2020-12-15 07:07:29 +0000

    Initial parsing and setup for day 15

--- 2020/src/day-15.py
+++ 2020/src/day-15.py
@@ -4,46 +4,48 @@ import argparse
 import sys
 
 # Check correct usage
-parser = argparse.ArgumentParser(description="Check your Passport.")
+parser = argparse.ArgumentParser(description="Check your Memory.")
 parser.add_argument('input', metavar='input', type=str,
-                    help='Password list input.')
+                    help='Numbers game input.')
 args = parser.parse_args()
 
-passes = []
+turns = []
 
 
 def main():
     parseInput(args.input)
 
     # Part 1
-    findSeats()
+    takeTurns(2020)
 
     # Part 2
 
     # Debug
-    printSeats()
+    printTurns()
 
 
 # Parse the input file
 def parseInput(inp):
-    global passes
+    global turns
     try:
-        passes_fh = open(inp, 'r')
+        nums_fh = open(inp, 'r')
     except IOError:
         sys.exit("Unable to open input file: " + inp)
 
-    for line in passes_fh:
-        passes.append(line.strip("\n"))
+    line = nums_fh.readline()
+    turns = [int(i) for i in line.strip("\n").split(',')]
 
 
 # For each pass, identify its seat
-def findSeats():
+def takeTurns(limit):
     return True
 
 
-def printSeats():
-    for item in passes:
-        print(item)
+def printTurns():
+    print(f"0: {turns[0]}", end="")
+    for turn, num in enumerate(turns, 1):
+        print(f", {turn}: {num}", end="")
+    print("")
 
 
 if __name__ == "__main__":
