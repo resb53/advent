0792352897191c259d1d9daebc0a5ccf371e1dfc
resb53 <resb53.git@gmail.com>
2020-12-16 07:20:27 +0000

    Solution to day 16, part 1

---
+++ 2020/src/day-16.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+import re
+
+# Check correct usage
+parser = argparse.ArgumentParser(description="Check your Train Ticket.")
+parser.add_argument('input', metavar='input', type=str,
+                    help='Ticket list input.')
+args = parser.parse_args()
+
+rules = {}
+myticket = []
+tickets = []
+errors = []
+
+
+def main():
+    parseInput(args.input)
+    uber = uberrule()
+
+    # Part 1
+    print(findErrors(uber))
+
+    # Part 2
+
+    # Debug
+    # printTickets()
+
+
+# Parse the input file
+def parseInput(inp):
+    global rules, myticket, tickets
+    try:
+        tickets_fh = open(inp, 'r')
+    except IOError:
+        sys.exit("Unable to open input file: " + inp)
+
+    mode = 'rules'
+
+    for line in tickets_fh:
+        line = line.strip("\n")
+
+        if line == 'your ticket:':
+            mode = 'myticket'
+        elif line == 'nearby tickets:':
+            mode = 'tickets'
+        else:
+            if line != '':
+                if mode == 'rules':
+                    match = re.match(r"^([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)$", line, re.I)
+                    rules[match.group(1)] = [(int(match.group(2)), int(match.group(3))),
+                                             (int(match.group(4)), int(match.group(5)))]
+                elif mode == 'myticket':
+                    myticket = [int(i) for i in line.split(',')]
+                else:
+                    ticket = [int(i) for i in line.split(',')]
+                    tickets.append(ticket)
+
+
+# Find single set of conditions to satisfy all rules
+def uberrule():
+    minval = 100
+    maxval = 900
+    # Looking at data show the 2 groups overlap, so just need a min and max
+    for rule in rules:
+        for pair in rules[rule]:
+            if pair[0] < minval:
+                minval = pair[0]
+            if pair[1] > maxval:
+                maxval = pair[1]
+
+    return (minval, maxval)
+
+
+# For each pass, identify its seat
+def findErrors(uber):
+    global errors
+
+    # ALL incorrect values (regardless of anything else)
+    for ticket in tickets:
+        for val in ticket:
+            if val < uber[0] or val > uber[1]:
+                errors.append(val)
+
+    return sum(errors)
+
+
+def printTickets():
+    for item in tickets:
+        print(item)
+
+
+if __name__ == "__main__":
+    main()
