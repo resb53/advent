79e02f9a558e9a1b377a74b5be1ff5583402f248
resb53 <resb53.git@gmail.com>
2020-12-23 14:39:38 +0000

    Solution to day 23, part 1

---
+++ 2020/src/day-23.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+
+# Check correct usage
+parser = argparse.ArgumentParser(description="Check your Cups.")
+parser.add_argument('input', metavar='input', type=str,
+                    help='Crab Cup list input.')
+args = parser.parse_args()
+
+cups = []
+current = 0
+
+
+def main():
+    parseInput(args.input)
+
+    # Part 1
+    for turn in range(100):
+        # print("".join([str(i) for i in cups]))
+        moveCups()
+
+    print("".join([str(i) for i in cups]))
+        
+    # Part 2
+
+    # Debug
+    # printCups()
+
+
+# Parse the input file
+def parseInput(inp):
+    global cups, current
+    try:
+        cups_fh = open(inp, 'r')
+    except IOError:
+        sys.exit("Unable to open input file: " + inp)
+
+    line = cups_fh.readline().strip("\n")
+    cups = [int(i) for i in line]
+    current = cups[0]
+
+
+# For each pass, identify its seat
+def moveCups():
+    global cups, current
+
+    curpos = cups.index(current)
+
+    pickup = [
+        (curpos + 1) % 9,
+        (curpos + 2) % 9,
+        (curpos + 3) % 9,
+    ]
+
+    heldcups = [cups[i] for i in pickup]
+    # print(heldcups)
+
+    dest = (current - 1) % 9
+    if dest == 0:
+        dest = 9
+
+    while dest in heldcups:
+        dest = (dest - 1) % 9
+        if dest == 0:
+            dest = 9
+
+    # print(dest)
+
+    for i in heldcups:
+        cups.remove(i)
+
+    destind = cups.index(dest)
+
+    for i, v in enumerate(heldcups, start=1):
+        cups.insert(destind + i, v)
+
+    curpos = cups.index(current)
+    nextpos = (curpos + 1) % 9
+    current = cups[nextpos]
+
+
+def printCups():
+    print(cups)
+
+
+if __name__ == "__main__":
+    main()
