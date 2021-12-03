d8b2da835241ffe6e594901eaf551f10d33b75ff
resb53 <resb53.git@gmail.com>
2021-12-03 07:49:30 +0000

    First attempt at day 3 part 2

--- 2021/src/day-03.py
+++ 2021/src/day-03.py
@@ -45,26 +45,58 @@ def processData():
             gamma += '1'
             epsilon += '0'
 
-    gamma = int(gamma, 2)
-    epsilon = int(epsilon, 2)
+    gammadec = int(gamma, 2)
+    epsilondec = int(epsilon, 2)
 
-    print(f"Gamma: {gamma}, Epsilon {epsilon}")
-    print(f"Solution 1: {gamma * epsilon}")
+    print(f"Gamma: {gammadec}, Epsilon {epsilondec}")
+    print(f"Solution 1: {gammadec * epsilondec}")
+
+    return (gamma, epsilon)
 
 
 # Process harder
-def processMore():
-    return False
+def processMore(gamma, epsilon):
+    oxy = data.copy()
+    co2 = data.copy()
+
+    # Oxygen
+    oxydel = []
+    for i, bit in enumerate(gamma):
+        if (len(oxy) > 1):
+            for j, val in enumerate(oxy):
+                if (val[i] != bit):
+                    oxydel.append(j)
+            for k in range(len(oxydel)-1, -1, -1):
+                oxy.pop(oxydel[k])
+            oxydel = []
+    
+    # CO2 Scrubbing
+    co2del = []
+    for i, bit in enumerate(epsilon):
+        if (len(co2) > 1):
+            for j, val in enumerate(co2):
+                if (val[i] != bit):
+                    co2del.append(j)
+            for k in range(len(co2del)-1, -1, -1):
+                co2.pop(co2del[k])
+            co2del = []
+    
+    oxydec = int(oxy[0], 2)
+    co2dec = int(co2[0], 2)
+
+    print(f"Oxygen: {oxydec}, CO2 Scrubbing: {co2dec}")
+    print(f"Solution 2: {oxydec * co2dec}")
+
 
 
 def main():
     parseInput(args.input)
 
     # Part 1
-    processData()
+    (g, e) = processData()
 
     # Part 2
-    processMore()
+    processMore(g, e)
 
 
 if __name__ == "__main__":
