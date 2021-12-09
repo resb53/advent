841f56a62723178f72288fbe6ba57547c949d6d8
resb53 <resb53.git@gmail.com>
2021-12-09 07:16:19 +0000

    Solution to day 9, part 2

--- 2021/src/day-09.py
+++ 2021/src/day-09.py
@@ -10,6 +10,9 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 grid = {}
+lows = []
+seen = []
+basin = {}
 
 
 # Parse the input file
@@ -40,14 +43,39 @@ def findLows():
                 if grid[check] <= grid[pos]:
                     low = False
         if low:
+            lows.append(pos)
             risk += 1 + grid[pos]
 
     print(f"Solution to Part 1: {risk}")
 
 
-# Process harder
-def processMore():
-    return False
+# Expand basins from a point
+def expandBasin(pos):
+    size = 0
+    for check in ((pos - 1j), (pos - 1), (pos + 1j), (pos + 1)):
+        if check in grid:
+            if check not in seen:
+                if grid[check] != 9:
+                    # print(f"Expanding into {check}...")
+                    seen.append(check)
+                    size += 1
+                    size += expandBasin(check)
+    return size
+
+
+# Find basin sizes for low points
+def findBasins():
+    # Work back through low points, and expand out. Mark other lows seen to skip.
+    for pos in lows:
+        if pos not in seen:
+            seen.append(pos)
+            # print(f"Expanding from {pos}...")
+            basin[pos] = 1
+            basin[pos] += expandBasin(pos)
+
+    sizes = sorted(basin.values())
+
+    print(f"Solution to Part 2: {sizes[-1] * sizes[-2] * sizes[-3]}")
 
 
 def main():
@@ -57,7 +85,7 @@ def main():
     findLows()
 
     # Part 2
-    processMore()
+    findBasins()
 
 
 if __name__ == "__main__":
