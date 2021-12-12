278d854a4108c486d37abc8700bee9382cc8389b
resb53 <resb53.git@gmail.com>
2021-12-12 08:16:55 +0000

    Use sets instead of duplicate checks to speed up

--- 2021/src/day-12.py
+++ 2021/src/day-12.py
@@ -31,26 +31,20 @@ def findRoutes(plot, plots):
     newplots = []
     for next in routes[plot[-1]]:
         if next not in plot:
-            newplot = plot.copy()
+            newplot = list(plot)
             newplot.append(next)
             newplots.append(newplot)
         elif next in plot and next.isupper():
-            newplot = plot.copy()
+            newplot = list(plot)
             newplot.append(next)
             newplots.append(newplot)
         elif next in plot[0] and plot.count(next) < 2:
-            newplot = plot.copy()
+            newplot = list(plot)
             newplot.append(next)
             newplots.append(newplot)
 
-    # Check for repeat routes
-    for check in newplots:
-        repeat = False
-        for validate in plots:
-            if check[1:] == validate[1:]:
-                repeat = True
-        if not repeat:
-            plots.append(check)
+    for newplot in newplots:
+        plots.add(tuple(newplot))
 
     for x in newplots:
         findRoutes(x, plots)
@@ -60,9 +54,9 @@ def main():
     parseInput(args.input)
 
     # Part 1
-    plots = []
+    plots = set()
     for x in routes["start"]:
-        plots.append(["*", "start", x])
+        plots.add(("*", "start", x))
 
     journeys = plots.copy()
     for plot in journeys:
@@ -75,7 +69,7 @@ def main():
     print(f"Solution to part 1: {count}")
 
     # Part 2
-    plots = []
+    plots = set()
     smalls = []
     for loc in routes:
         if loc.islower() and loc != "start" and loc != "end":
@@ -83,18 +77,20 @@ def main():
 
     for x in routes["start"]:
         for double in smalls:
-            plots.append([double + "*", "start", x])
+            plots.add((double + "*", "start", x))
 
     journeys = plots.copy()
 
     for plot in journeys:
-        findRoutes(plot, plots)       
+        findRoutes(plot, plots)
 
     count = 0
     for route in plots:
         if route[-1] == "end":
             count += 1
     print(f"Solution to part 2: {count}")
+    for x in plots:
+        print(x)
 
 
 if __name__ == "__main__":
