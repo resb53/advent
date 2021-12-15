f74ea76231db8b55394c9bb76e53cc48c6b1e796
resb53 <resb53.git@gmail.com>
2021-12-15 06:22:21 +0000

    Solution to day 15, part 1

--- 2021/src/day-15.py
+++ 2021/src/day-15.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+import networkx as nx
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Parse some data.")
@@ -9,7 +10,8 @@ parser.add_argument('input', metavar='input', type=str,
                     help='Input data file.')
 args = parser.parse_args()
 
-data = []
+G = nx.DiGraph()
+grid = {}
 
 
 # Parse the input file
@@ -19,14 +21,43 @@ def parseInput(inp):
     except IOError:
         sys.exit("Unable to open input file: " + inp)
 
+    x = 0
+    y = 0
+
     for line in input_fh:
-        data.append(line.strip("\n"))
+        line = line.strip("\n")
+        for val in line:
+            G.add_node((x, y))
+            grid[(x, y)] = int(val)
+            x += 1
+        x = 0
+        y += 1
+
+    # Create network of nodes between grid positions to use those as edges
+    maxx = int(max(grid.keys(), key=lambda k: k[0])[0])
+    maxy = int(max(grid.keys(), key=lambda k: k[1])[1])
+
+    # Set edges attributes right and down for each node unless node is right or bottom edge
+    for node in G.nodes:
+        if node[0] != maxx:
+            noderight = (node[0] + 1, node[1])
+            G.add_edge(node, noderight, weight=grid[noderight])
+            G.add_edge(noderight, node, weight=grid[node])
+        if node[1] != maxy:
+            nodedown = (node[0], node[1] + 1)
+            G.add_edge(node, nodedown, weight=grid[nodedown])
+            G.add_edge(nodedown, node, weight=grid[node])
+
+    return (maxx, maxy)
 
 
 # For each pass, identify its seat
-def processData():
-    for element in data:
-        print(f"{element}")
+def shortestPath(start, end):
+    sp = nx.shortest_path(G, source=start, target=end, weight="weight")
+    cost = 0
+    for x in sp[1:]:
+        cost += grid[x]
+    print(cost)
 
 
 # Process harder
@@ -35,10 +66,10 @@ def processMore():
 
 
 def main():
-    parseInput(args.input)
+    (maxx, maxy) = parseInput(args.input)
 
     # Part 1
-    processData()
+    shortestPath((0, 0), (maxx, maxy))
 
     # Part 2
     processMore()
