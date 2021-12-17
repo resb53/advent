6aa4c2144e343d55ce0e8095cf2db5d204eeb514
resb53 <resb53.git@gmail.com>
2021-12-17 06:35:36 +0000

    Less mathsy part 1

--- 2021/src/day-17.py
+++ 2021/src/day-17.py
@@ -6,14 +6,31 @@ target = {"x": (207, 263), "y": (-63, -115)}
 
 # Find initial trajectory that ends in target
 def calculateTrajectory():
-    # (Max target y) - 1 initial velocity - calculate max height reached (y*(y+1)/2)
-    maxy = (((target["y"][1] * -1) - 1) * (target["y"][1] * -1))/2
-    print(f"Solution to part 1: {maxy}")
+    # (Max target y) - 1 initial velocity
+    maxy = (target["y"][1] * -1) - 1
+    posy = 0
+    lasty = None
+    while posy != lasty:
+        lasty = posy
+        posy += maxy
+        maxy -= 1
+
+    print(f"Solution to part 1: {posy}")
 
 
 # Process harder
 def calculateAll():
-    # Min velx must reach minx
+    # Min velx must reach minx, do in reverse
+    posx = 0
+    velx = 0
+    while posx < target["x"][0]:
+        velx += 1
+        posx += velx
+    minx = velx
+    maxx = target["x"][1]
+    # Values for y:
+    miny = target["y"][1]
+    maxy = (target["y"][1] * -1) - 1
 
     return False
 
