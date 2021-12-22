9c69a07f277bcad53910bce9f900fbfb362039d9
resb53 <resb53.git@gmail.com>
2021-12-22 07:59:39 +0000

    Attempt to process intersections

--- 2021/src/day-22.py
+++ 2021/src/day-22.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+from collections import Counter
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Parse some data.")
@@ -22,6 +23,10 @@ def parseInput(inp):
     for line in input_fh:
         box = {}
         (state, instr) = line.strip("\n").split(" ")
+        if state == "on":
+            state = 1
+        else:
+            state = 0
         instr = instr.split(",")
         for r in instr:
             (d, vals) = r.split("=")
@@ -47,12 +52,53 @@ def startReactor():
     return len(cores)
 
 
+# New approach for part 2, track blocks and calculate any intersections
+def cubeIntersects():
+    # Ignore off regions
+    cores = Counter()
+
+    for switch in data:
+        if switch[0]:
+            minx = min(switch[1]["x"])
+            maxx = max(switch[1]["x"])
+            miny = min(switch[1]["y"])
+            maxy = max(switch[1]["y"])
+            minz = min(switch[1]["z"])
+            maxz = max(switch[1]["z"])
+            reg = (minx, maxx, miny, maxy, minz, maxz)
+            cores[reg] += 1
+        doIntersects(cores, reg)
+
+    return cores
+
+
+# Prevent double counting of intersects, delete for cubes already considered
+def doIntersects(cores, reg):
+    newregs = Counter()
+    for region in cores:
+        xbigmin = max(reg[0], region[0])
+        xsmolmax = min(reg[1], region[1])
+        ybigmin = max(reg[2], region[2])
+        ysmolmax = min(reg[3], region[3])
+        zbigmin = max(reg[4], region[4])
+        zsmolmax = min(reg[5], region[5])
+
+        if zbigmin <= zsmolmax:
+            if ybigmin <= ysmolmax:
+                if xbigmin <= xsmolmax:
+                    newregs[(xbigmin, xsmolmax, ybigmin, ysmolmax, zbigmin, zsmolmax)] -= 1
+
+    cores.update(newregs)
+
+
 def main():
     parseInput(args.input)
 
     # Part 1
     print(f"Solution to part 1: {startReactor()}")
 
+    # Part 2
+    print(f"Solution to part 2: {cubeIntersects()}")
 
 
 
