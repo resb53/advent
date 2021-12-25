42bc4ecb7ae262e87b70d6b5885aa9ee16a64879
resb53 <resb53.git@gmail.com>
2021-12-25 06:09:43 +0000

    Solution to day 25, part 1

--- 2021/src/day-24.py
+++ 2021/src/day-24.py
@@ -2,7 +2,6 @@
 
 import argparse
 import sys
-from collections import defaultdict
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Parse some data.")
@@ -32,141 +31,6 @@ def parseInput(inp):
     return instr
 
 
-def doinp(reg, v, a):
-    reg[v] = a
-
-
-def doadd(reg, a, b):
-    if type(b) == int:
-        reg[a] = reg[a] + b
-    else:
-        reg[a] = reg[a] + reg[b]
-
-
-def domul(reg, a, b):
-    if type(b) == int:
-        reg[a] = reg[a] * b
-    else:
-        reg[a] = reg[a] * reg[b]
-
-
-def dodiv(reg, a, b):
-    if type(b) == int:
-        reg[a] = reg[a] // b
-    else:
-        reg[a] = reg[a] // reg[b]
-
-
-def domod(reg, a, b):
-    if type(b) == int:
-        reg[a] = reg[a] % b
-    else:
-        reg[a] = reg[a] % reg[b]
-
-
-def doeql(reg, a, b):
-    if type(b) == int:
-        reg[a] = 1 if reg[a] == b else 0
-    else:
-        reg[a] = 1 if reg[a] == reg[b] else 0
-
-
-# Run loaded program
-def runProgram(instr, vals):
-    vals = vals.copy()
-    register = {"w": 0, "x": 0, "y": 0, "z": 0}
-
-    for i, ins in enumerate(instr):
-        # print(f"{i}: {register}")
-        if ins[0] == 'inp':
-            doinp(register, ins[1], vals.pop(0))
-        elif ins[0] == "add":
-            doadd(register, ins[1], ins[2])
-        elif ins[0] == "mul":
-            domul(register, ins[1], ins[2])
-        elif ins[0] == "div":
-            dodiv(register, ins[1], ins[2])
-        elif ins[0] == "mod":
-            domod(register, ins[1], ins[2])
-        elif ins[0] == "eql":
-            doeql(register, ins[1], ins[2])
-        # if i % 18 == 0:
-        #     print(register["z"])
-
-    return register["z"]
-
-
-# Analyse Ops
-def analyseOps(instr):
-    analyse = instr.copy()
-    # Manual observation, blocks of 18 similar instructions. Check this:
-    n = 1
-    while len(analyse) > 0:
-        block, analyse = analyse[0:18], analyse[18:]
-        print(f"Block {n}:")
-        # Read next value into W
-        if block[0] != ["inp", "w"]:
-            print(f"{n}, 0: {block[0]}")
-        # Set X = 0
-        if block[1] != ["mul", "x", 0]:
-            print(f"{n}, 1: {block[1]}")
-        # Set X = prevZ
-        if block[2] != ["add", "x", "z"]:
-            print(f"{n}, 2: {block[2]}")
-        # X = prevZ mod 26
-        if block[3] != ["mod", "x", 26]:
-            print(f"{n}, 3: {block[3]}")
-        # Z = prevZ OR prevZ // 26
-        if block[4][0:2] != ["div", "z"]:
-            print(f"{n}, 4: {block[4]}")
-        else:
-            print(f"4: {block[4][2]}", end="")
-        # X = (Z mod 26 + A)
-        if block[5][0:2] != ["add", "x"]:
-            print(f"{n}, 5: {block[5]}")
-        else:
-            print(f", 5: {block[5][2]}", end="")
-        # X = 1 if (Z mod 26 + A) == W else 0
-        if block[6] != ["eql", "x", "w"]:
-            print(f"{n}, 6: {block[6]}")
-        # X = 1 if (Z mod 26 + A) != W else 0
-        if block[7] != ["eql", "x", 0]:
-            print(f"{n}, 7: {block[7]}")
-        # Y = 0
-        if block[8] != ["mul", "y", 0]:
-            print(f"{n}, 8: {block[8]}")
-        # Y = 25
-        if block[9] != ["add", "y", 25]:
-            print(f"{n}, 9: {block[9]}")
-        # Y = 25 if (Z mod 26 + A) != W else 0
-        if block[10] != ["mul", "y", "x"]:
-            print(f"{n}, 10: {block[10]}")
-        # Y = 26 if (Z mod 26 + A) != W else 1
-        if block[11] != ["add", "y", 1]:
-            print(f"{n}, 11: {block[11]}")
-        # Z = (26 * Z) if (Z mod 26 + A) != W else Z
-        if block[12] != ["mul", "z", "y"]:
-            print(f"{n}, 12: {block[12]}")
-        # Y = 0
-        if block[13] != ["mul", "y", 0]:
-            print(f"{n}, 13: {block[13]}")
-        # Y = W
-        if block[14] != ["add", "y", "w"]:
-            print(f"{n}, 14: {block[14]}")
-        # Y = W + B
-        if block[15][0:2] != ["add", "y"]:
-            print(f"{n}, 15: {block[15]}")
-        else:
-            print(f", 15: {block[15][2]}")
-        # Y = (W + B) if (Z mod 26 + A) != W else 0
-        if block[16] != ["mul", "y", "x"]:
-            print(f"{n}, 16: {block[16]}")
-        # Z = ((26 * Z) + (W + B)) if (Z mod 26 + A) != W else Z
-        if block[17] != ["add", "z", "y"]:
-            print(f"{n}, 17: {block[17]}")
-        n += 1
-
-
 def simplifyInput(instr):
     analyse = instr.copy()
     simplified = []
@@ -192,7 +56,7 @@ def runSimply(instr, input):
 
 
 def traceStates(instr):
-    zeds = defaultdict(int)
+    zeds = {}
     for f in range(1, 10):
         z = runSimply(instr, [f])
         zeds[z] = f
@@ -205,17 +69,18 @@ def traceStates(instr):
 
 
 def addInput(zeds, instr):
-    newzeds = defaultdict(int)
-    dupes = 0
+    newzeds = {}
+
     for f in zeds.values():
         for g in range(1, 10):
             val = int(str(f) + str(g))
             vals = [int(i) for i in list(str(val))]
             z = runSimply(instr, vals)
-            if val > newzeds[z]:
-                newzeds[z] = val
+            if z in newzeds:
+                if val < newzeds[z]:
+                    newzeds[z] = val
             else:
-                dupes += 1
+                newzeds[z] = val
 
     return newzeds
 
@@ -225,7 +90,7 @@ def main():
     simpl = simplifyInput(instr)
 
     # Part 1
-    print(f"Solution to part 1: {traceStates(simpl)}")
+    print(f"Solution to part 2: {traceStates(simpl)}")
 
 
 if __name__ == "__main__":
---
+++ 2021/src/day-25.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+# Check correct usage
+parser = argparse.ArgumentParser(description="Parse some data.")
+parser.add_argument('input', metavar='input', type=str,
+                    help='Input data file.')
+args = parser.parse_args()
+
+maxes = {'x': 0, 'y': 0}
+
+
+# Parse the input file
+def parseInput(inp):
+    try:
+        input_fh = open(inp, 'r')
+    except IOError:
+        sys.exit("Unable to open input file: " + inp)
+
+    grid = {}
+    loc = 0j
+
+    for line in input_fh:
+        line = line.strip("\n")
+        for x in line:
+            grid[loc] = x
+            loc += 1
+        maxes["x"] = int(loc.real)
+        loc -= loc.real
+        loc += 1j
+    maxes["y"] = int(loc.imag)
+
+    return grid
+
+
+# Iterate through time moving seas cucumbers
+def moveFauna(grid):
+    moved = 1
+    steps = 0
+
+    while moved > 0:
+        moved = 0
+
+        # East first
+        moved += moveAll(grid, ">")
+
+        # Then South
+        moved += moveAll(grid, "v")
+
+        steps += 1
+
+    return steps
+
+
+def moveAll(grid, x):
+    movers = []
+    for loc in grid:
+        if grid[loc] == x:
+            if canMove(grid, loc):
+                movers.append(loc)
+    updateGrid(grid, movers)
+    return len(movers)
+
+
+def canMove(grid, loc):
+    check = wrapGrid(grid, loc)    
+    if grid[check] == ".":
+        return True
+    else:
+        return False
+
+
+def wrapGrid(grid, loc):
+    dest = -1
+    if grid[loc] == ">":
+        dest = loc + 1
+        if dest.real > maxes["x"] - 1:
+            dest -= dest.real
+    elif grid[loc] == "v":
+        dest = loc + 1j
+        if dest.imag > maxes["y"] - 1:
+            dest -= dest.imag * 1j
+    return dest
+
+
+def updateGrid(grid, movers):
+    for loc in movers:
+        dest = wrapGrid(grid, loc)
+        grid[dest] = grid[loc]
+        grid[loc] = "."
+
+
+def printGrid(grid):
+    for y in range(maxes["y"]):
+        for x in range(maxes["x"]):
+            print(grid[x + y * 1j], end="")
+        print()
+
+
+def main():
+    grid = parseInput(args.input)
+
+    # Part 1
+    print(f"Solution to part 1: {moveFauna(grid)}")
+
+
+if __name__ == "__main__":
+    main()
