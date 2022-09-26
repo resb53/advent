ca92175dec156ddfdea6844a538647d558f6f787
resb53 <resb53.git@gmail.com>
2022-09-26 07:22:21 +0100

    Prep for 2022

---
+++ 2022/src/code-stub.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+# Check correct usage
+parser = argparse.ArgumentParser(description="Parse some data.")
+parser.add_argument('input', metavar='input', type=str,
+                    help='Input data file.')
+args = parser.parse_args()
+
+data = []
+
+
+# Parse the input file
+def parseInput(inp):
+    try:
+        input_fh = open(inp, 'r')
+    except IOError:
+        sys.exit("Unable to open input file: " + inp)
+
+    for line in input_fh:
+        data.append(line.strip("\n"))
+
+
+# For each pass, identify its seat
+def processData():
+    for element in data:
+        print(f"{element}")
+
+
+# Process harder
+def processMore():
+    return False
+
+
+def main():
+    parseInput(args.input)
+
+    # Part 1
+    processData()
+
+    # Part 2
+    processMore()
+
+
+if __name__ == "__main__":
+    main()
