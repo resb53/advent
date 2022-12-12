d3fb927f909cc1c3feec334e27fb49b296391c30
resb53 <resb53.git@gmail.com>
2022-12-12 06:24:41 +0000

    Solution to day 12, part 1

---
+++ 2022/src/day-12.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+import networkx as nx
+
+# Check correct usage
+parser = argparse.ArgumentParser(description="Parse some data.")
+parser.add_argument('input', metavar='input', type=str,
+                    help='Input data file.')
+args = parser.parse_args()
+
+grid = {}
+vectors = [-1j, 1, 1j, -1]
+start = 0j
+end = 0j
+
+
+# Parse the input file
+def parseInput(inp):
+    try:
+        input_fh = open(inp, 'r')
+    except IOError:
+        sys.exit("Unable to open input file: " + inp)
+
+    global start
+    global end
+    row = 0
+
+    for line in input_fh:
+        for i, x in enumerate(list(line.strip("\n"))):
+            if x == "S":
+                grid[i + row * 1j] = "a"
+                start = i + row * 1j
+            elif x == "E":
+                grid[i + row * 1j] = "z"
+                end = i + row * 1j
+            else:
+                grid[i + row * 1j] = x
+        row += 1
+
+
+# Create networkx map from grid
+def createMap():
+    G = nx.DiGraph()
+
+    # Create nodes
+    for pos in grid.keys():
+        G.add_node(pos)
+
+    # For each node identify moveable edges
+    for pos in grid.keys():
+        for dest in getEdges(pos):
+            G.add_edge(pos, dest)
+
+    return G
+
+
+# Calculate movable edges for a given node
+def getEdges(pos):
+    edges = []
+
+    for dest in [pos + x for x in vectors]:
+        if dest in grid:
+            if ord(grid[dest]) <= ord(grid[pos]) + 1:
+                edges.append(dest)
+
+    return edges
+
+
+# Find shortest path from start to end
+def processData(G):
+    print(f"Part 1: {len(nx.shortest_path(G, start, end)) - 1}")
+
+
+# Process harder
+def processMore():
+    return False
+
+
+def main():
+    parseInput(args.input)
+    G = createMap()
+
+    # Part 1
+    processData(G)
+
+    # Part 2
+    processMore()
+
+
+if __name__ == "__main__":
+    main()
