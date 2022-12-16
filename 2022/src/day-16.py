f0d06efb4ac8d2a0035605f7ae6968de04978502
resb53 <resb53.git@gmail.com>
2022-12-16 12:17:59 +0000

    Condensed cave map, work out how to minimise search space for paths

--- 2022/src/day-16.py
+++ 2022/src/day-16.py
@@ -5,6 +5,7 @@ import sys
 import networkx as nx
 import re
 import copy
+from itertools import combinations, permutations
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Parse some data.")
@@ -29,66 +30,37 @@ def parseInput(inp):
             data.append([match[1], int(match[2]), match[3].split(", ")])
 
 
-# Build cave map
+# Build cave map - it's never worth stopping at or turning on a valve when the valve = 0
 def caveMap():
     G = nx.Graph()
+    zeroes = []
 
     for paths in data:
         G.add_node(paths[0], flow=paths[1])
-        G.add_nodes_from(paths[2])
-        G.add_edges_from([(paths[0], x) for x in paths[2]])
+        G.add_edges_from([(paths[0], x, {"weight": 1}) for x in paths[2]])
+
+    for node in G.nodes():
+        if node != "AA" and G.nodes[node]["flow"] == 0:
+            zeroes.append(node)
+
+    for node in zeroes:
+        for connect in combinations(G.neighbors(node), 2):
+            if connect not in G.edges():
+                G.add_edge(connect[0], connect[1], weight=nx.shortest_path_length(G, connect[0], connect[1], "weight"))
+            else:
+                print(f"Check {connect}")
+        G.remove_node(node)
+
+    print(G.nodes())
+    for edge in G.edges():
+        print(f'{edge}: {G.edges[edge]["weight"]}')
 
     return G
 
 
-# For each pass, identify its seat
-def processData(caves: nx.DiGraph):
-
-    # List of actions, current released pressure, current exhaust rate, opened valves
-    actions = [[["AA"], 0, 0, []]]
-
-    # BFS through all routes until time passed == 30
-    for _ in range(10):
-        updated = []
-        for route in actions:
-            pos = route[0][-1]
-            if pos == "open":
-                pos = route[0][-2]
-
-            # If valve closed, could open it
-            if pos not in route[3]:
-                nextstep = copy.deepcopy(route[0])
-                nextstep.append("open")
-                exhaust = route[1] + route[2]
-                newflow = route[2] + caves.nodes[pos]["flow"]
-                opened = copy.deepcopy(route[3])
-                opened.append(pos)
-                updated.append([nextstep, exhaust, newflow, opened])
-
-            # Could move to any connected room
-            for neighbour in caves[pos]:
-                nextstep = copy.deepcopy(route[0])
-                nextstep.append(neighbour)
-                exhaust = route[1] + route[2]
-                opened = copy.deepcopy(route[3])
-                updated.append([nextstep, exhaust, route[2], route[3]])
-
-            # Could do nothing
-            nextstep = copy.deepcopy(route[0])
-            nextstep.append(pos)
-            exhaust = route[1] + route[2]
-            opened = copy.deepcopy(route[3])
-            updated.append([nextstep, exhaust, route[2], route[3]])
-
-        actions = updated
-
-    mostflow = 0
-
-    for x in actions:
-        if x[1] > mostflow:
-            mostflow = x[1]
-
-    print(f"Part 1: {mostflow}")
+# Find route that releases the most pressure in 30 minutes
+def processData(caves: nx.Graph):
+    print(list(nx.all_pairs_shortest_path(caves)))
 
 
 # Process harder
