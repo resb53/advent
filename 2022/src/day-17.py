9a6ab5baa7a2ecfbe5ab20f1b53b24d7f0abe1cc
resb53 <resb53.git@gmail.com>
2022-12-17 07:57:28 +0000

    Can't move right or left if shape in way

--- 2022/src/day-17.py
+++ 2022/src/day-17.py
@@ -13,10 +13,10 @@ data = []
 
 shapes = [
     [2, 3, 4, 5],  # -
-    [3, 2+1j, 2+1j, 4+1j],  # +
+    [3, 2+1j, 3+1j, 4+1j, 3+2j],  # +
     [2, 3, 4, 4+1j, 4+2j],  # ⅃
     [2, 2+1j, 2+2j, 2+3j],  # |
-    [2, 3, 2+1j, 2+2j]  # □
+    [2, 3, 2+1j, 3+1j]  # □
 ]
 
 
@@ -36,6 +36,51 @@ def generateShape():
         i += 1
 
 
+# Drop shape
+def dropShape(floor, wind, shape):
+    base = max(floor) * 1j + 4j
+    falling = [x + base for x in next(shape)]
+    leftedge = min([int(x.real) for x in falling])
+    rightedge = max([int(x.real) for x in falling])
+
+    while True:
+        print(falling)
+        # Wind blows
+        blow = next(wind)
+        print(blow)
+        if blow == "<":
+            if leftedge > 0:
+                falling = [x - 1 for x in falling]
+                leftedge = min([int(x.real) for x in falling])
+                rightedge = max([int(x.real) for x in falling])
+        else:
+            if rightedge < 6:
+                falling = [x + 1 for x in falling]
+                leftedge = min([int(x.real) for x in falling])
+                rightedge = max([int(x.real) for x in falling])
+        print(falling)
+        print("v")
+        # Block falls
+        stops = False
+        for x in falling:
+            if int(x.imag) == floor[int(x.real)] + 1:
+                stops = True
+        if stops:
+            break
+        else:
+            falling = [x - 1j for x in falling]
+
+    # Update floor
+    for x in range(len(floor)):
+        newy = floor[x]
+        for y in falling:
+            if int(y.real) == x and int(y.imag) > newy:
+                newy = int(y.imag)
+        floor[x] = newy
+
+    print(floor)
+
+
 # Parse the input file
 def parseInput(inp):
     try:
@@ -49,11 +94,13 @@ def parseInput(inp):
 
 # For each pass, identify its seat
 def processData():
+    floor = [0] * 7
     wind = generateWind()
     shape = generateShape()
-    for _ in range(2):
-        print(next(wind))
-        print(next(shape))
+
+    for count in range(3):
+        dropShape(floor, wind, shape)
+
 
 
 # Process harder
