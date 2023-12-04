d47ba850b94fa11910be6a531dcfa52f4e6d3ae1
resb53 <resb53.git@gmail.com>
2023-12-04 07:36:13 +0000

    Solution to day 1, part 2

--- 2023/src/day-01.py
+++ 2023/src/day-01.py
@@ -27,7 +27,7 @@ def parseInput(inp):
         data.append(line.strip("\n"))
 
 
-# For each pass, identify its seat
+# For each line, identify first and last digits
 def processData():
     total = 0
 
@@ -47,22 +47,77 @@ def processData():
         # print(value)
         total += int(value)
 
-    print(f"Part 1: {total}")
+    return total
 
 
-# Process harder
+# Replace text with digits then redo part 1
 def processMore():
-    return False
+    total = 0
+
+    for line in data:
+        value = ''
+        index = 0
+
+        while len(value) == 0:
+            if line[index] in "123456789":
+                value += line[index]
+            elif line[index:index+3] == "one":
+                value += "1"
+            elif line[index:index+3] == "two":
+                value += "2"
+            elif line[index:index+5] == "three":
+                value += "3"
+            elif line[index:index+4] == "four":
+                value += "4"
+            elif line[index:index+4] == "five":
+                value += "5"
+            elif line[index:index+3] == "six":
+                value += "6"
+            elif line[index:index+5] == "seven":
+                value += "7"
+            elif line[index:index+5] == "eight":
+                value += "8"
+            elif line[index:index+4] == "nine":
+                value += "9"
+            index += 1
+
+        index = len(line)
+        while len(value) == 1:
+            if line[index-1] in "123456789":
+                value += line[index-1]
+            elif line[index-3:index] == "one":
+                value += "1"
+            elif line[index-3:index] == "two":
+                value += "2"
+            elif line[index-5:index] == "three":
+                value += "3"
+            elif line[index-4:index] == "four":
+                value += "4"
+            elif line[index-4:index] == "five":
+                value += "5"
+            elif line[index-3:index] == "six":
+                value += "6"
+            elif line[index-5:index] == "seven":
+                value += "7"
+            elif line[index-5:index] == "eight":
+                value += "8"
+            elif line[index-4:index] == "nine":
+                value += "9"
+            index -= 1
+
+        total += int(value)
+
+    return total
 
 
 def main():
     parseInput(args.input)
 
     # Part 1
-    processData()
+    print(f"Part 1: {processData()}")
 
     # Part 2
-    processMore()
+    print(f"Part 2: {processMore()}")
 
 
 if __name__ == "__main__":
