38120c6f5aea1aa23979700bc4ef3442a481c57c
resb53 <resb53.git@gmail.com>
2023-12-07 09:22:09 +0000

    Solution to day 7, part 2

--- 2023/src/day-07.py
+++ 2023/src/day-07.py
@@ -11,6 +11,7 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 hands = []
+jhands = []
 
 
 class InvalidCardException(Exception):
@@ -176,6 +177,182 @@ class Hand:
             return False
 
 
+class Jcard:
+    order = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
+
+    def __new__(cls, value):
+        if value in Jcard.order:
+            return super().__new__(cls)
+        else:
+            raise InvalidCardException(f"Jcard must have a value in {Jcard.order}.")
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+    def __lt__(self, other):
+        return Jcard.order.index(self.value) < Jcard.order.index(other.value)
+
+    def __le__(self, other):
+        return Jcard.order.index(self.value) <= Jcard.order.index(other.value)
+
+    def __gt__(self, other):
+        return Jcard.order.index(self.value) > Jcard.order.index(other.value)
+
+    def __ge__(self, other):
+        return Jcard.order.index(self.value) >= Jcard.order.index(other.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
+class Jhand:
+    types = ["HC", "1P", "2P", "3oak", "FH", "4oak", "5oak"]
+
+    def __new__(cls, value):
+        if type(value) is not list:
+            raise InvalidHandException("Jhand must be a list of 5 Cards.")
+        else:
+            if len(value) != 5:
+                raise InvalidHandException("Jhand must contain exactly 5 Cards.")
+            for crd in value:
+                if type(crd) is not Jcard:
+                    raise InvalidHandException("Jhand must only contain Cards.")
+            return super().__new__(cls)
+
+    def __init__(self, value):
+        self.contents = value
+
+        # Determine most powerful hand type
+        sorthand = sorted(value)
+        groups = []
+        unique = []
+        for k, g in groupby(sorthand):
+            groups.append(list(g))
+            unique.append(k)
+
+        if Jcard("J") in unique:
+            jcount = len(groups[0])  # J is weakest Jcard
+            # Make strongest type by adding J to any largest group
+            if jcount != 5:
+                jgroup = groups.pop(0)
+                unique.pop(0)
+
+                largestcount = 0
+                largestindex = 0
+
+                for ix, grp in enumerate(groups):
+                    if len(grp) >= largestcount:
+                        largestcount = len(grp)
+                        largestindex = ix
+
+                groups[largestindex].extend(jgroup)
+
+        if len(groups) == 1:
+            self.type = "5oak"
+
+        elif len(groups) == 2:
+            if len(groups[0]) == 4 or len(groups[1]) == 4:
+                self.type = "4oak"
+            else:
+                self.type = "FH"
+
+        elif len(groups) == 3:
+            if len(groups[0]) == 3 or len(groups[1]) == 3 or len(groups[2]) == 3:
+                self.type = "3oak"
+            else:
+                self.type = "2P"
+
+        elif len(groups) == 4:
+            self.type = "1P"
+
+        else:
+            self.type = "HC"
+
+    def __str__(self):
+        return str([str(x) for x in self.contents])
+
+    def __lt__(self, other):
+        if self.type != other.type:
+            return Jhand.types.index(self.type) < Jhand.types.index(other.type)
+        else:
+            if self.contents[0] != other.contents[0]:
+                return self.contents[0] < other.contents[0]
+            elif self.contents[1] != other.contents[1]:
+                return self.contents[1] < other.contents[1]
+            elif self.contents[2] != other.contents[2]:
+                return self.contents[2] < other.contents[2]
+            elif self.contents[3] != other.contents[3]:
+                return self.contents[3] < other.contents[3]
+            elif self.contents[4] != other.contents[4]:
+                return self.contents[4] < other.contents[4]
+            else:
+                return False
+
+    def __le__(self, other):
+        if self.type != other.type:
+            return Jhand.types.index(self.type) <= Jhand.types.index(other.type)
+        else:
+            if self.contents[0] != other.contents[0]:
+                return self.contents[0] <= other.contents[0]
+            elif self.contents[1] != other.contents[1]:
+                return self.contents[1] <= other.contents[1]
+            elif self.contents[2] != other.contents[2]:
+                return self.contents[2] <= other.contents[2]
+            elif self.contents[3] != other.contents[3]:
+                return self.contents[3] <= other.contents[3]
+            elif self.contents[4] != other.contents[4]:
+                return self.contents[4] <= other.contents[4]
+            else:
+                return False
+
+    def __gt__(self, other):
+        if self.type != other.type:
+            return Jhand.types.index(self.type) > Jhand.types.index(other.type)
+        else:
+            if self.contents[0] != other.contents[0]:
+                return self.contents[0] > other.contents[0]
+            elif self.contents[1] != other.contents[1]:
+                return self.contents[1] > other.contents[1]
+            elif self.contents[2] != other.contents[2]:
+                return self.contents[2] > other.contents[2]
+            elif self.contents[3] != other.contents[3]:
+                return self.contents[3] > other.contents[3]
+            elif self.contents[4] != other.contents[4]:
+                return self.contents[4] > other.contents[4]
+            else:
+                return False
+
+    def __ge__(self, other):
+        if self.type != other.type:
+            return Jhand.types.index(self.type) >= Jhand.types.index(other.type)
+        else:
+            if self.contents[0] != other.contents[0]:
+                return self.contents[0] >= other.contents[0]
+            elif self.contents[1] != other.contents[1]:
+                return self.contents[1] >= other.contents[1]
+            elif self.contents[2] != other.contents[2]:
+                return self.contents[2] >= other.contents[2]
+            elif self.contents[3] != other.contents[3]:
+                return self.contents[3] >= other.contents[3]
+            elif self.contents[4] != other.contents[4]:
+                return self.contents[4] >= other.contents[4]
+            else:
+                return False
+
+    def __eq__(self, other):
+        if self.contents[0] == other.contents[0] and \
+           self.contents[1] == other.contents[1] and \
+           self.contents[2] == other.contents[2] and \
+           self.contents[3] == other.contents[3] and \
+           self.contents[4] == other.contents[4]:
+            return True
+        else:
+            return False
+
+
 # Parse the input file
 def parseInput(inp):
     try:
@@ -187,9 +364,11 @@ def parseInput(inp):
         cards, bet = line.rstrip().split()
         hand = Hand([Card(x) for x in cards])
         hands.append([hand, int(bet)])
+        jhand = Jhand([Jcard(x) for x in cards])
+        jhands.append([jhand, int(bet)])
 
 
-# For each pass, identify its seat
+# Find winnings for standard rules
 def processData():
     winnings = 0
     sorthands = sorted(hands)
@@ -198,9 +377,13 @@ def processData():
     return winnings
 
 
-# Process harder
+# Find winnings for Joker J's
 def processMore():
-    return False
+    winnings = 0
+    sortjhands = sorted(jhands)
+    for rank, hand in enumerate(sortjhands):
+        winnings += (rank+1) * hand[1]
+    return winnings
 
 
 def main():
