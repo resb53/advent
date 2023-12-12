2e8faf21e176d0b3d98a6c6a93a13f938234c680
resb53 <resb53.git@gmail.com>
2023-12-12 19:47:43 +0000

    Starter for day 12

--- 2023/src/day-12.py
+++ 2023/src/day-12.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+import re
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Parse some data.")
@@ -10,6 +11,17 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 data = []
+
+'''
+Plan:
+- Create recursive function that ultimately returns 1 if a valid combination is found, or 0 if not, and sums these.
+- Start from the left, find the first valid match of the first valid group.
+- Kick off one recursion using that, and proceeding
+    - Returning 0 if it doesn't complete.
+    - Return 1 if it gets to the end of a valid line.
+- Move past the first ? and start a new recursion pattern.
+- Move past a # and move to next group if valid count (Need to think this through more)
+'''
 
 
 # Parse the input file
@@ -20,14 +32,33 @@ def parseInput(inp):
         sys.exit("Unable to open input file: " + inp)
 
     for line in input_fh:
-        data.append(line.rstrip())
+        (line, arr) = line.rstrip().split()
+        arr = [int(x) for x in arr.split(",")]
+        data.append([line, arr])
 
 
-# For each pass, identify its seat
+# Calculate number of arrangements for a given pattern
+def arrange(line, arr):
+    print(line, arr)
+    # Locate first possible location from left
+    poss = re.match(r"^\.*[?#]{" + str(arr[0]) + "}(?!#)", line)
+    # Distance to next considered point
+    seen = re.match(r"^\.*(?:#+|\?)", line)
+
+    if poss is not None:
+        print(f"Poss: {poss[0]}")
+    if seen is not None:
+        print(f"Seen: {seen[0]}")
+
+    return 1
+
+
+# For each line of spring, work out how many possible arrangements
 def processData():
+    arrangements = 0
     for element in data:
-        print(f"{element}")
-    return False
+        arrangements += arrange(element[0], element[1])
+    return arrangements
 
 
 # Process harder
