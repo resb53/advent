bc52ca02b8ca43b36e6347763934dde5b684446e
resb53 <resb53.git@gmail.com>
2023-12-14 07:35:30 +0000

    Solution to day 14, part 1

--- 2023/src/day-14.py
+++ 2023/src/day-14.py
@@ -9,7 +9,8 @@ parser.add_argument('input', metavar='input', type=str,
                     help='Input data file.')
 args = parser.parse_args()
 
-data = []
+squares = []
+rounds = []
 
 
 # Parse the input file
@@ -19,15 +20,72 @@ def parseInput(inp):
     except IOError:
         sys.exit("Unable to open input file: " + inp)
 
+    maxx = 0
+    y = 0
     for line in input_fh:
-        data.append(line.rstrip())
+        line = line.rstrip()
+        if maxx == 0:
+            maxx = len(line)
+        for x, val in enumerate(line):
+            if val == "#":
+                squares.append(complex(x, y))
+            elif val == "O":
+                rounds.append(complex(x, y))
+        y += 1
+
+    return (maxx, y)
 
 
-# For each pass, identify its seat
-def processData():
-    for element in data:
-        print(f"{element}")
-    return False
+# Tilt the deck
+def tilt(direction, bounds):
+    # Sort to focus on ones nearest the downward edge first
+    match direction:
+        case "N":
+            rounds.sort(key=lambda x: x.imag)
+            for i, pos in enumerate(rounds):
+                nearObst = -1
+                obstacles = [x for x in squares if x.real == pos.real]
+                obstacles.extend([x for x in rounds if x.real == pos.real])
+                for obs in obstacles:
+                    if obs.imag < pos.imag and obs.imag > nearObst:
+                        nearObst = obs.imag
+                rounds[i] = complex(pos.real, nearObst + 1)
+
+        case "E":
+            rounds.sort(key=lambda x:x.real, reverse=True)
+        case "S":
+            rounds.sort(key=lambda x:x.imag, reverse=True)
+        case "W":
+            rounds.sort(key=lambda x:x.real)
+
+
+# Print the grind
+def printGrid(bounds):
+    for y in range(bounds[1]):
+        for x in range(bounds[0]):
+            pos = complex(x, y)
+            if pos in squares:
+                print("#", end="")
+            elif pos in rounds:
+                print("O", end="")
+            else:
+                print(".", end="")
+        print()
+
+
+# Calculate strain
+def calculateStrain(bounds):
+    strain = 0
+    for x in rounds:
+        strain += bounds[1] - x.imag
+    return int(strain)
+
+
+# Find the strain when all rocks moved North
+def processData(bounds):
+    # Tilt North
+    tilt("N", bounds)
+    return calculateStrain(bounds)
 
 
 # Process harder
@@ -36,10 +94,10 @@ def processMore():
 
 
 def main():
-    parseInput(args.input)
+    bounds = parseInput(args.input)
 
     # Part 1
-    print(f"Part 1: {processData()}")
+    print(f"Part 1: {processData(bounds)}")
 
     # Part 2
     print(f"Part 2: {processMore()}")
