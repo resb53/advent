82953583cacea64aaaa8cd9490c2da390c8832b9
resb53 <resb53.git@gmail.com>
2023-12-19 22:21:01 +0000

    Solution to day 17, part 2

--- 2023/src/day-17.py
+++ 2023/src/day-17.py
@@ -37,25 +37,48 @@ def parseInput(inp):
 
 
 # Shortest path without more than 3 movements inthe same direction
-def shortestPaths(start, bounds):
+def shortestPaths(start, bounds, part):
     pq = [(0, start, 0, 0)]  # heat, pos, dir, straights
     distances = {}
 
     while len(pq) > 0:
         heat, pos, drn, straights = heappop(pq)
+
         for move in moves:
             newpos = pos + move
             if newpos.real < 0 or newpos.real > bounds[0] or newpos.imag < 0 or newpos.imag > bounds[1]:
                 continue
+
             newstraights = straights
-            if move == drn:
-                newstraights += 1
-            elif move == -drn:
-                continue
-            else:
-                newstraights = 1
-            if newstraights > 3:
-                continue
+
+            if part == 1:
+                if move == drn:
+                    newstraights += 1
+                elif pos == nplex(0, 0):
+                    newstraights = 1
+                elif move == -drn:
+                    continue
+                else:
+                    newstraights = 1
+
+                if newstraights > 3:
+                    continue
+
+            elif part == 2:
+                if move == drn:
+                    newstraights += 1
+                elif pos == nplex(0, 0):
+                    newstraights = 1
+                elif move == -drn:
+                    continue
+                elif newstraights < 4:
+                    continue
+                else:
+                    newstraights = 1
+
+                if newstraights > 10:
+                    continue
+
             newheat = heat + weights[newpos]
             if newpos not in distances:
                 distances[newpos] = {(move, newstraights): newheat}
@@ -64,23 +87,28 @@ def shortestPaths(start, bounds):
                 distances[newpos][(move, newstraights)] = newheat
                 heappush(pq, (newheat, newpos, move, newstraights))
 
-    # for x in range(0, bounds[0]+1):
-    #     for y in range(0, bounds[1]+1):
-    #         print(f"{nplex(x, y)}: {distances[nplex(x, y)]}")
-
-    return min(distances[nplex(bounds[0], bounds[1])].values())
+    if part == 1:
+        return min(distances[nplex(bounds[0], bounds[1])].values())
+    elif part == 2:
+        stoppable = []
+        for k, v in distances[nplex(bounds[0], bounds[1])].items():
+            if k[1] >= 4:
+                stoppable.append(v)
+        return min(stoppable)
 
 
 # For each pass, identify its seat
 def processData():
     start = nplex(0, 0)
     bounds = (int(max([x.real for x in weights.keys()])), int(max([y.imag for y in weights.keys()])))
-    return shortestPaths(start, bounds)
+    return shortestPaths(start, bounds, 1)
 
 
 # Process harder
 def processMore():
-    return False
+    start = nplex(0, 0)
+    bounds = (int(max([x.real for x in weights.keys()])), int(max([y.imag for y in weights.keys()])))
+    return shortestPaths(start, bounds, 2)
 
 
 def main():
