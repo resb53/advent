323743efb6ba5965838c6b3f5279fd5899dbefd0
resb53 <resb53.git@gmail.com>
2023-12-23 20:18:44 +0000

    Solution to day 23, part 2

--- 2023/src/day-23.py
+++ 2023/src/day-23.py
@@ -10,8 +10,7 @@ parser.add_argument('input', metavar='input', type=str,
                     help='Input data file.')
 args = parser.parse_args()
 
-G = nx.DiGraph()
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(20000)
 
 
 # Parse the input file
@@ -22,6 +21,8 @@ def parseInput(inp):
         sys.exit("Unable to open input file: " + inp)
 
     # Setup nodes
+    G = nx.DiGraph()
+
     y = 0
     for line in input_fh:
         line = line.rstrip()
@@ -66,67 +67,81 @@ def parseInput(inp):
                     if move == -1:
                         G.add_edge(node, node + move)
 
-    return [start for start in G.nodes() if start.imag == 0][0], [end for end in G.nodes() if end.imag == y - 1][0]
+    return G, [start for start in G.nodes() if start.imag == 0][0], [end for end in G.nodes() if end.imag == y - 1][0]
 
 
 # Find a unique route through the maze
-def getRoutes(start, finish, route, routes):
+def getRoutes(G, start, finish, route, routes):
     for x in nx.neighbors(G, start):
         if x not in route:
             localroute = route.copy()
             localroute.append(x)
+            if "weight" in G[start][x]:
+                localroute[0] += G[start][x]["weight"]
             if x == finish:
                 routes.append(localroute)
             else:
-                getRoutes(x, finish, localroute, routes)
+                getRoutes(G, x, finish, localroute, routes)
 
 
 # Find most scenic route that doesn't repeat a node
-def processData(start, finish):
+def processData(G, start, finish):
     routes = []
-    route = []
-    getRoutes(start, finish, route, routes)
-    longest = max([len(x) for x in routes])
-    '''
-    for i in range(longest):
-        print(f"{i+1}:", end="")
-        for r in routes:
-            if i < len(r):
-                print(f"\t{r[i]}".expandtabs(10), end="")
-            else:
-                print("\t".expandtabs(10), end="")
-        print()
-    '''
+    route = [0]
+    getRoutes(G, start, finish, route, routes)
+    longest = max([len(x) for x in routes]) - 1
 
     return longest
 
 
+# Collapse edges between "hallway" nodes
+def collapse(G):
+    criticalNodes = [node for node in G.nodes() if len(list(nx.neighbors(G, node))) != 2]
+    for node in criticalNodes:
+        destinations = list(G.neighbors(node))
+        for dest in destinations:
+            while dest not in criticalNodes:
+                # Find and collapse path to next critical node
+                weight = G[node][dest]["weight"]
+                subs = [x for x in G.neighbors(dest) if x != node][0]
+                nx.contracted_edge(G, (node, dest), self_loops=False, copy=False)
+                G[node][subs]["weight"] += weight
+                dest = subs
+
+
 # With reverse slopes
-def processMore(start, finish):
+def processMore(G, start, finish):
+
+    NG = nx.Graph()
+    NG.add_nodes_from(G)
+
     # Setup edges
     adj = [-1j, 1, 1j, -1]
-    for node in G:
+    for node in NG:
         for move in adj:
-            if node + move in G:
-                G.add_edge(node, node + move)
-                G.add_edge(node + move, node)
+            if node + move in NG:
+                NG.add_edge(node, node + move, weight=1)
 
-    # Traverse
+    # Collapse Graph
+    collapse(NG)
+
+    # Calculate Route
     routes = []
-    route = []
-    getRoutes(start, finish, route, routes)
+    route = [0]
+    getRoutes(NG, start, finish, route, routes)
+    longest = max([x[0] for x in routes])
 
-    return max([len(x) for x in routes])
+    return longest
 
 
 def main():
-    start, finish = parseInput(args.input)
+    G, start, finish = parseInput(args.input)
 
     # Part 1
-    print(f"Part 1: {processData(start, finish)}")
+    print(f"Part 1: {processData(G, start, finish)}")
 
     # Part 2
-    print(f"Part 2: {processMore(start, finish)}")
+    print(f"Part 2: {processMore(G, start, finish)}")
 
 
 if __name__ == "__main__":
