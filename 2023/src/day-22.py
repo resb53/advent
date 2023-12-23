0a5e44db4191ef4d65b2ed1b0be346ed7d4cf771
resb53 <resb53.git@gmail.com>
2023-12-23 21:42:01 +0000

    Hoping for more accurate implementation with Class

--- 2023/src/day-22.py
+++ 2023/src/day-22.py
@@ -13,7 +13,115 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 stack = defaultdict(dict)
-bricks = defaultdict(list)
+bricks = []
+
+
+class Brick():
+    def __init__(self, start, finish):
+        # Set name
+        while True:
+            name = "".join(choices(string.ascii_uppercase + string.digits, k=3))
+            if name not in [x.name for x in bricks]:
+                break
+        self.name = name
+
+        # Initialise
+        self.supports = set()  # Blocks that currently support this one
+        self.suspends = set()  # Blocks that this supports
+
+        # Get dimension
+        delta = None
+        for i in range(3):
+            if start[i] != finish[i]:
+                delta = i
+                break
+        # Load positions
+        match delta:
+            case 0:
+                self.orientation = "horizontal"
+                self.pos = [(x, start[1], start[2])
+                            for x in range(min((start[0], finish[0])), max((start[0], finish[0])) + 1)]
+            case 1:
+                self.orientation = "horizontal"
+                self.pos = [(start[0], y, start[2])
+                            for y in range(min((start[1], finish[1])), max((start[1], finish[1])) + 1)]
+            case 2:
+                self.orientation = "vertical"
+                self.pos = [(start[0], start[1], z)
+                            for z in range(min((start[2], finish[2])), max((start[2], finish[2])) + 1)]
+            case _:
+                self.orientation = "vertical"
+                self.pos = [(start[0], start[1], start[2])]
+
+        # Update world
+        self.insert()
+        bricks.append(self)
+
+    def __str__(self):
+        return f"{self.name}: {self.pos[0]} -> {self.pos[-1]}"
+
+    def __repr__(self):
+        return self.name
+
+    def __lt__(self, other):
+        lowzself = min([pos[2] for pos in self.pos])
+        lowzother = min([pos[2] for pos in other.pos])
+        return lowzself < lowzother
+
+    def insert(self):
+        for (x, y, z) in self.pos:
+            if complex(x, y) not in stack[z]:
+                stack[z][complex(x, y)] = self
+
+    def update(self):
+        self.supports.clear()
+        self.suspends.clear()
+        if self.orientation == "vertical":
+            self.pos.sort(key=lambda x: x[2])
+            if complex(self.pos[0][0], self.pos[0][1]) in stack[self.pos[0][2] - 1]:
+                self.supports.add(stack[self.pos[0][2] - 1][complex(self.pos[0][0], self.pos[0][1])])
+            if complex(self.pos[-1][0], self.pos[-1][1]) in stack[self.pos[-1][2] + 1]:
+                self.suspends.add(stack[self.pos[-1][2] + 1][complex(self.pos[-1][0], self.pos[-1][1])])
+        else:
+            for (x, y, z) in self.pos:
+                if complex(x, y) in stack[z - 1]:
+                    self.supports.add(stack[z - 1][complex(x, y)])
+                if complex(x, y) in stack[z + 1]:
+                    self.suspends.add(stack[z + 1][complex(x, y)])
+
+    def drop(self):
+        if len(self.supports) == 0:
+            if self.orientation == "vertical":
+                x = self.pos[0][0]
+                y = self.pos[0][1]
+                lowz = min([pos[2] for pos in self.pos])
+                for z in range(lowz - 1, 0, -1):
+                    if complex(x, y) in stack[z]:
+                        self.descend(lowz - z - 1)
+                        return
+                self.descend(lowz - 1)
+            else:
+                for z in range(self.pos[0][2] - 1, 0, -1):
+                    for pos in self.pos:
+                        if complex(pos[0], pos[1]) in stack[z]:
+                            self.descend(pos[2] - z - 1)
+                            return
+                self.descend(self.pos[0][2] - 1)
+
+    def descend(self, dist):
+        if dist > 0:
+            for (x, y, z) in self.pos:
+                try:
+                    stack[z].pop(complex(x, y))
+                except KeyError:
+                    sys.exit(f"Block {(x, y, z)} not found in Stack.")
+            for i, (x, y, z) in enumerate(self.pos):
+                if z - dist < 1:
+                    raise ValueError(f"Cannot descend to {(x, y, z - dist)}")
+                self.pos[i] = (x, y, z - dist)
+            self.insert()
+            for brick in bricks:
+                brick.update()
 
 
 # Parse the input file
@@ -25,115 +133,29 @@ def parseInput(inp):
 
     for line in input_fh:
         line = line.rstrip()
-        start, finish = line.split("~")
-        start = [int(x) for x in start.split(",")]
-        finish = [int(x) for x in finish.split(",")]
+        start, finish = [[int(x) for x in y.split(",")] for y in line.split("~")]
+        _ = Brick(start, finish)
 
-        name = "".join(choices(string.ascii_uppercase + string.digits, k=3))
-        if name in bricks or name + "v" in bricks:
-            name = "".join(choices(string.ascii_uppercase + string.digits, k=3))
-
-        # Find orientation
-        for i in range(3):
-            if start[i] != finish[i]:
-                match i:
-                    case 0:
-                        for x in range(min((start[i], finish[i])), max((start[i], finish[i])) + 1):
-                            stack[start[2]][complex(x, start[1])] = name
-                            bricks[name].append((x, start[1], start[2]))
-                    case 1:
-                        for y in range(min((start[i], finish[i])), max((start[i], finish[i])) + 1):
-                            stack[start[2]][complex(start[0], y)] = name
-                            bricks[name].append((start[0], y, start[2]))
-                    case 2:  # Vertical
-                        for z in range(min((start[i], finish[i])), max((start[i], finish[i])) + 1):
-                            stack[z][complex(start[0], start[1])] = name + "v"
-                            bricks[name + "v"].append((start[0], start[1], z))
-
-
-# Drop bricks like Tetris
-def tetris():
-    # print(stack)
-    ordered_bricks = list(bricks.keys())
-    ordered_bricks.sort(key=lambda x: bricks[x][0][2])
-    for brick in ordered_bricks:
-        # print(f"{brick}: {bricks[brick]}")
-        drop = 0
-        if brick[-1] == "v":
-            for z in range(bricks[brick][0][2] - 1, 0, -1):
-                # print(f"Checking {(bricks[brick][0][0], bricks[brick][0][1], z)}...")
-                if complex(bricks[brick][0][0], bricks[brick][0][1]) in stack[z]:
-                    drop = bricks[brick][0][2] - z - 1
-                    break
-        else:
-            for z in range(bricks[brick][0][2] - 1, 0, -1):
-                for pos in bricks[brick]:
-                    # print(f"Checking {(pos[0], pos[1], z)}...")
-                    if complex(pos[0], pos[1]) in stack[z]:
-                        drop = pos[2] - z - 1
-                        break
-                else:
-                    continue
-                break
-        # print(f"Drop: {drop}")
-        if drop > 0:
-            newbrick = []
-            for pos in bricks[brick]:
-                newbrick.append((pos[0], pos[1], pos[2] - drop))
-                stack[pos[2]].pop(complex(pos[0], pos[1]))
-                stack[pos[2] - drop][complex(pos[0], pos[1])] = brick
-            bricks[brick] = newbrick
-            # print(f"Dropped to: {bricks[brick]}")
-
-
-# Work through bricks to see if they are the only support for another. Return count of those that don't
-def zappable():
-    # print(stack)
-    zap = 0
     for brick in bricks:
-        # print(f"{brick}: {bricks[brick]}")
-        if brick[-1] == "v":
-            if complex(bricks[brick][-1][0], bricks[brick][-1][1]) not in stack[bricks[brick][-1][2] + 1]:
-                zap += 1
-        else:
-            supporting = []
-            for pos in bricks[brick]:
-                if complex(pos[0], pos[1]) in stack[pos[2] + 1]:
-                    supporting.append(stack[pos[2] + 1][complex(pos[0], pos[1])])
-            if len(supporting) == 0:
-                zap += 1
-            else:
-                # print(f"Supporting: {supporting}")
-                zappable = True
-                for suspended in supporting:
-                    # print(f"{suspended} supported by: {getSupports(suspended)}")
-                    if len(getSupports(suspended)) == 1:
-                        zappable = False
-                        break
-                if zappable:
-                    # print("Zap!")
-                    zap += 1
-
-    return zap
+        brick.update()
 
 
-# Get supports for a given block
-def getSupports(block):
-    supports = []
-    if block[-1] == "v":
-        if complex(bricks[block][0][0], bricks[block][0][1]) in stack[bricks[block][0][2] - 1]:
-            supports.append(stack[bricks[block][0][2] - 1][complex(bricks[block][0][0], bricks[block][0][1])])
-    else:
-        for pos in bricks[block]:
-            if complex(pos[0], pos[1]) in stack[pos[2] - 1]:
-                supports.append(stack[pos[2] - 1][complex(pos[0], pos[1])])
-    return supports
-
-
-# Identify number of bricks that can be safely disintegrated
+# For each pass, identify its seat
 def processData():
-    tetris()
-    return zappable()
+    bricks.sort()
+    for brick in bricks:
+        brick.drop()
+    # Count bricks that support nothing, or support a brick with other supports
+    count = 0
+    for brick in bricks:
+        if len(brick.suspends) == 0:
+            count += 1
+        else:
+            for sus in brick.suspends:
+                if len(sus.supports) > 1:
+                    count += 1
+                    break
+    return count
 
 
 # Process harder
