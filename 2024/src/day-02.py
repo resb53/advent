d484112ea56793470cf0e1b0567c027e3ab886b4
Robert Bogie <robert.bogie@anaplan.com>
2024-12-08 18:01:53 +0000

    Solution to day 2, part 2

--- 2024/src/day-02.py
+++ 2024/src/day-02.py
@@ -25,21 +25,44 @@ def parseInput(inp):
 
 # For each report, scan the levels to see if they're safe
 def processData():
-    safe = 0
+    safep1 = 0
+    safep2 = 0
     for report in data:
         scan = []
         for lvl in range(len(report) - 1):
             scan.append(report[lvl] - report[lvl + 1])
-        # Check if always decreasing and no 0's
-        if all(x < 0 for x in scan) or all(x > 0 for x in scan):
-            # Check if all absolute values less than or equal to 3
-            if all(abs(x) <= 3 for x in scan):
-                safe += 1
-    return safe
+        # Check the scan
+        if checkScan(scan):
+            safep1 += 1
+            safep2 += 1
+        # Try any unsafe reports if allowing for dampening
+        else:
+            if dampen(report):
+                safep2 += 1
+    return safep1, safep2
 
 
-# Process harder
-def processMore():
+# Check a scan of a report and return true if safe, false if not
+def checkScan(scan):
+    # Check if always decreasing and no 0's
+    if all(x < 0 for x in scan) or all(x > 0 for x in scan):
+        # Check if all absolute values less than or equal to 3
+        if all(abs(x) <= 3 for x in scan):
+            return True
+    return False
+
+
+# Recheck scan, but allow for dampening - i.e. drop each value until one returns true, else return false
+def dampen(rpt):
+    for i in range(len(rpt)):
+        test = rpt.copy()
+        test.pop(i)
+        # Scan new report
+        scan = []
+        for lvl in range(len(test) - 1):
+            scan.append(test[lvl] - test[lvl + 1])
+        if checkScan(scan):
+            return True
     return False
 
 
@@ -47,10 +70,11 @@ def main():
     parseInput(args.input)
 
     # Part 1
-    print(f"Part 1: {processData()}")
+    p1, p2 = processData()
+    print(f"Part 1: {p1}")
 
     # Part 2
-    print(f"Part 2: {processMore()}")
+    print(f"Part 2: {p2}")
 
 
 if __name__ == "__main__":
