9446987fadfa9894c6dc51cdaa89db3a743ba23c
Robert Bogie <robert.bogie@anaplan.com>
2024-12-10 08:32:20 +0000

    Solution to day 9, part 2

--- 2024/src/day-09.py
+++ 2024/src/day-09.py
@@ -10,7 +10,8 @@ parser.add_argument('input', metavar='input', type=str,
 args = parser.parse_args()
 
 data = []
-
+files = []  # (start, length) - id is pos in array
+space = []  # (startpos, length)
 
 # Parse the input file
 def parseInput(inp):
@@ -23,13 +24,18 @@ def parseInput(inp):
         digits = [int(x) for x in line.rstrip()]
         free = False
         id = 0
+        pos = 0
         for x in digits:
             if not free:
                 data.extend([id] * x)
+                files.append((pos, x))
+                pos += x
                 free = not free
                 id += 1
             else:
                 data.extend(["."] * x)
+                space.append((pos, x))
+                pos += x
                 free = not free
 
 
@@ -63,9 +69,49 @@ def checksum(fs):
     return total
 
 
-# Process harder
+# Move full files, not just blocks
 def processMore():
-    return False
+    for id in range(len(files)-1, -1, -1):
+        start, size = files[id]
+        # Find left-most space which could contain file
+        for i, x in enumerate(space):
+            if x[1] >= size and x[0] < start:
+                # Move file into space
+                if x[1] > size:
+                    space[i] = (x[0] + size, x[1] - size)
+                else:
+                    space.remove(x)
+                files[id] = (x[0], size)
+                # Add space back in!
+                space.append((start, size))
+                cleanup(space)
+                break
+    return blockcheck(files)
+
+
+# Cleanup available space records
+def cleanup(records):
+    # Arrange
+    records.sort(key=lambda x: x[0])
+    # Amalgamate
+    for i in range(len(records)-2, -1, -1):
+        left = records[i]
+        right = records[i+1]
+        if left[0] + left[1] == right[0]:
+            update = (left[0], left[1] + right[1])
+            records.pop(i + 1)
+            records[i] = update
+
+
+# Calculate checksum of blocked files
+def blockcheck(fs):
+    total = 0
+    for id, file in enumerate(fs):
+        loc, size = file
+        for x in range(size):
+            total += loc * id
+            loc += 1
+    return total
 
 
 def main():
