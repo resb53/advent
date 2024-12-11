745616abe9f47e38c6b9d35570e7a9228d650577
resb53 <resb53.git@gmail.com>
2024-12-11 07:21:51 +0000

    Solution to day 11, part 1

--- 2024/src/day-11.py
+++ 2024/src/day-11.py
@@ -20,29 +20,40 @@ def parseInput(inp):
         sys.exit("Unable to open input file: " + inp)
 
     for line in input_fh:
-        data.append(line.rstrip())
+        data.extend([int(x) for x in line.rstrip().split()])
 
 
-# For each pass, identify its seat
-def processData():
-    for element in data:
-        print(f"{element}")
-    return False
+# For each blink, update the stones
+def processData(n, stones):
+    for i in range(1, n+1):
+        stones = blink(stones)
+    return len(stones)
 
 
-# Process harder
-def processMore():
-    return False
+# Carry out stone operations for a blink
+def blink(stones):
+    newstones = []
+    for stone in stones:
+        if stone == 0:
+            newstones.append(1)
+        elif len(str(stone)) % 2 == 0:
+            hl = len(str(stone)) // 2
+            a = int(str(stone)[0:hl])
+            b = int(str(stone)[hl:])
+            newstones.extend([a, b])
+        else:
+            newstones.append(stone * 2024)
+    return newstones
 
 
 def main():
     parseInput(args.input)
 
     # Part 1
-    print(f"Part 1: {processData()}")
+    print(f"Part 1: {processData(25, data)}")
 
     # Part 2
-    print(f"Part 2: {processMore()}")
+    # print(f"Part 2: {processData(75, data)}")
 
 
 if __name__ == "__main__":
