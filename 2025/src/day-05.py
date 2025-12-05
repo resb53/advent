0c7f93d4abe3d6d57212d4c12a3ad1615f195e23
resb53 <resb53.git@gmail.com>
2025-12-05 08:52:13 +0000

    Solution to day 5, part 1

--- 2025/src/day-05.py
+++ 2025/src/day-05.py
@@ -9,7 +9,6 @@ parser.add_argument('input', metavar='input', type=str,
                     help='Input data file.')
 args = parser.parse_args()
 
-ranges = []
 data = []
 
 
@@ -20,11 +19,13 @@ def parseInput(inp):
     except IOError:
         sys.exit("Unable to open input file: " + inp)
 
+    ranges = []
+
     for line in input_fh:
         line = line.rstrip()
         if "-" in line:
             insert = -1
-            r = [int(x) for x in line.split("-")]
+            r = tuple(int(x) for x in line.split("-"))
             for i, x in enumerate(ranges):
                 if r[0] > x[0]:
                     continue
@@ -44,34 +45,56 @@ def parseInput(inp):
         elif len(line) > 0:
             data.append(int(line))
 
+    return ranges
+
 
 # Collapse fresh ranges
-def collapseRanges():
-    return 0
+def collapseRanges(ranges):
+    rng = []
+    a, b = ranges[0]
+
+    for r in ranges[1:]:
+        if r[0] > b + 1:
+            rng.append((a, b))
+            a, b = r
+        else:
+            b = max(b, r[1])
+
+    rng.append((a, b))
+
+    return rng
 
 
-# Find fresh ingredients
-def processData():
-    for r in ranges:
-        print(f"{r[0]} -> {r[1]}")
-    for element in data:
-        print(f"{element}")
-    return False
+# Count fresh ingredients
+def processData(ranges):
+    count = 0
+    rng = collapseRanges(ranges)
+
+    for x in data:
+        fresh = False
+        for r in rng:
+            if x >= r[0] and x <= r[1]:
+                fresh = True
+
+        if fresh:
+            count += 1
+
+    return count
 
 
 # Process harder
-def processMore():
+def processMore(ranges):
     return False
 
 
 def main():
-    parseInput(args.input)
+    ranges = parseInput(args.input)
 
     # Part 1
-    print(f"Part 1: {processData()}")
+    print(f"Part 1: {processData(ranges)}")
 
     # Part 2
-    print(f"Part 2: {processMore()}")
+    print(f"Part 2: {processMore(ranges)}")
 
 
 if __name__ == "__main__":
