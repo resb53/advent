28d2c701d1ede07a453a695f2f2e0da1803cddb9
resb53 <resb53.git@gmail.com>
2025-12-07 06:42:06 +0000

    Solution to day 7, part 2

--- 2025/src/day-07.py
+++ 2025/src/day-07.py
@@ -26,7 +26,10 @@ def parseInput(inp):
         x = 0
         line = line.rstrip()
         for char in line:
-            grid[x + y * 1j] = char
+            if char == ".":
+                grid[x + y * 1j] = 0
+            else:
+                grid[x + y * 1j] = char
             x += 1
         y += 1
 
@@ -47,24 +50,44 @@ def printGrid(grid):
 
 
 # Pass Tachyon beams through the field
-def processData(grid):
+def processData(g):
+    grid = g.copy()
     split = 0
 
     for y in range(1, bounds[1]):
         for x in range(bounds[0]):
-            if grid[x + y * 1j] != "^" and grid[x + (y-1) * 1j] in ["S", "|"]:
-                grid[x + y * 1j] = "|"
-            elif grid[x + y * 1j] == "^" and grid[x + (y-1) * 1j] == "|":
+            if grid[x + y * 1j] == 0 and grid[x + (y-1) * 1j] in ["S", 1]:
+                grid[x + y * 1j] = 1
+            if grid[x + y * 1j] == "^" and grid[x + (y-1) * 1j] == 1:
                 split += 1
-                grid[x-1 + y * 1j] = "|"
-                grid[x+1 + y * 1j] = "|"
+                grid[x-1 + y * 1j] = 1
+                grid[x+1 + y * 1j] = 1
 
     return split
 
 
 # Process harder
-def processMore(grid):
-    return False
+def processMore(g):
+    grid = g.copy()
+
+    for y in range(1, bounds[1]):
+        for x in range(bounds[0]):
+            if grid[x + y * 1j] != "^":
+                if grid[x + y * 1j] >= 0:
+                    if grid[x + (y-1) * 1j] == "S":
+                        grid[x + y * 1j] = 1
+                    elif grid[x + (y-1) * 1j] != "^" and grid[x + (y-1) * 1j] > 0:
+                        grid[x + y * 1j] += grid[x + (y-1) * 1j]
+            elif grid[x + y * 1j] == "^":
+                if grid[x + (y-1) * 1j] > 0:
+                    grid[x-1 + y * 1j] += grid[x + (y-1) * 1j]
+                    grid[x+1 + y * 1j] += grid[x + (y-1) * 1j]
+
+    routes = 0
+    for x in range(bounds[0]):
+        routes += grid[x + (bounds[1] - 1) * 1j]
+
+    return routes
 
 
 def main():
