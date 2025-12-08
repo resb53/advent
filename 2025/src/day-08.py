c4263e7cba9fc3864bffa5f3793cf7a7a83f8122
resb53 <resb53.git@gmail.com>
2025-12-08 09:06:19 +0000

    Solution to day 8, part 1

--- 2025/src/day-08.py
+++ 2025/src/day-08.py
@@ -2,6 +2,8 @@
 
 import argparse
 import sys
+from math import sqrt, pow, prod
+from itertools import combinations
 
 # Check correct usage
 parser = argparse.ArgumentParser(description="Parse some data.")
@@ -20,14 +22,55 @@ def parseInput(inp):
         sys.exit("Unable to open input file: " + inp)
 
     for line in input_fh:
-        data.append(line.rstrip())
+        line = line.rstrip()
+        data.append(tuple([int(x) for x in line.split(",")]))
 
 
-# For each pass, identify its seat
+# Find straight-line distance between two points
+def straightDistance(a, b):
+    return sqrt(pow((b[0] - a[0]), 2) + pow((b[1] - a[1]), 2) + pow((b[2] - a[2]), 2))
+
+
+# Connect circuits
+def connectCircuits(circuits, pair):
+    a = -1
+    b = -1
+
+    for i, x in enumerate(circuits):
+        if pair[0] in x:
+            a = i
+        if pair[1] in x:
+            b = i
+
+    if a == -1 or b == -1:
+        sys.exit(f"Error: Cannot find {pair} in circuits.")
+
+    if a == b:
+        # Already connected
+        return False
+    else:
+        circuits[a].extend(circuits[b])
+        circuits.pop(b)
+
+
+# Find shortest distance between points, connect the first 1000
 def processData():
-    for element in data:
-        print(f"{element}")
-    return False
+    distances = {}
+    circuits = [[x] for x in data]
+
+    for pair in combinations(data, 2):
+        distances[(pair[0], pair[1])] = straightDistance(pair[0], pair[1])
+
+    n = 0
+    for pair in sorted(distances, key=distances.get):
+        connectCircuits(circuits, pair)
+        n += 1
+        if n >= 1000:
+            break
+
+    circuits.sort(key=lambda x: len(x), reverse=True)
+
+    return prod([len(x) for x in circuits[0:3]])
 
 
 # Process harder
